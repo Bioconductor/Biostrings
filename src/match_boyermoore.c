80112eda2663026132ab3d15c87c44a672d84763
Herve Pages <hpages@fhcrc.org>
2007-02-13 11:12:45 +0000

    Some change in the terminology: the "Strong Good Suffix shifts" are now called the "Very Strong Good Suffix shifts" to reflect the fact that they differ (and are >=) from the usual Strong Good Suffix shifts. The "Good Right Shifts" are now called the "Matching Window shifts".
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@22607 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/match_boyermoore.c
+++ src/match_boyermoore.c
@@ -8,13 +8,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* I've turned off the GRS feature. Doesn't seem to give very good
- * results :-( To turned it on, set MAXNP_WITH_GRS to a positive integer
+/* I've turned off the "MWshift feature". Doesn't seem to give very good
+ * results :-( To turned it on, set MWSHIFT_NPMAX to a positive integer
  * (typically 128, 256 or 512, doesn't have to be a power of 2).
- * The original idea was that the "GRS feature" could boost the boyermoore()
+ * The original idea of this feature was to try to boost the boyermoore()
  * function when the pattern is small.
  */
-#define MAXNP_WITH_GRS	0
+#define MWSHIFT_NPMAX	0
 
 
 /****************************************************************************/
@@ -82,16 +82,16 @@ static void init_P0buffer(char *P, int nP)
 
 
 /****************************************************************************
- * The Strong Good Suffix shifts
- * =============================
+ * The Very Strong Good Suffix shifts
+ * ==================================
  */
 
-/* Contains the Strong Good Suffix shifts for current pattern P0. */
-static int *SGSshift_table = NULL;
+/* Contains the "Very Strong Good Suffix shifts" for current pattern P0. */
+static int *VSGSshift_table = NULL;
 
-/* SGSshift_table is a (256, P0buffer_length) matrix.
- * The layout of SGSshift_table is (only the values marked with an "x" will
- * be potentially used):
+/* VSGSshift_table is a 256 x P0buffer_length matrix.
+ * Its layout is (only the values marked with an "x" will be potentially
+ * used):
  *
  *           0 1 2 3 4 5 j
  *         0 x x x x - -
@@ -104,16 +104,13 @@ static int *SGSshift_table = NULL;
  * The "x" region is defined by 0 <= j < nP0
  */
 
-#define SGSshift(c, j)	(SGSshift_table[P0buffer_length * ((unsigned char) c) + j])
+#define VSGSshift(c, j)	(VSGSshift_table[P0buffer_length * ((unsigned char) c) + j])
 
-/* Dumb and slow.
- * TODO: Use something faster like Cole's preprocessing algo.
- */
-static int get_SGSshift(char c, int j)
+static int get_VSGSshift(char c, int j)
 {
 	int shift, k, k1, k2, length;
 
-	shift = SGSshift(c, j);
+	shift = VSGSshift(c, j);
 	if (shift != 0)
 		return shift;
 	for (shift = 1; shift < nP0; shift++) {
@@ -133,88 +130,89 @@ static int get_SGSshift(char c, int j)
 			break;
 	}
 	/* shift is nP0 when the "for" loop is not interrupted by "break" */
-	/*Rprintf("SGSshift(c=%c, j=%d) = %d\n", c, j, shift);*/
-	return SGSshift(c, j) = shift;
+	/*Rprintf("VSGSshift(c=%c, j=%d) = %d\n", c, j, shift);*/
+	return VSGSshift(c, j) = shift;
 }
 
-static void init_SGSshift_table()
+static void init_VSGSshift_table()
 {
 	int u, j;
 	char c;
 
-	if (P0buffer_init_status == -1 && SGSshift_table != NULL) {
-		free(SGSshift_table);
-		SGSshift_table = NULL;
+	if (P0buffer_init_status == -1 && VSGSshift_table != NULL) {
+		free(VSGSshift_table);
+		VSGSshift_table = NULL;
 	}
-	if (P0buffer_length != 0 && SGSshift_table == NULL) {
-		SGSshift_table = (int *) malloc(256 * P0buffer_length *
+	if (P0buffer_length != 0 && VSGSshift_table == NULL) {
+		VSGSshift_table = (int *) malloc(256 * P0buffer_length *
 						sizeof(int));
-		if (SGSshift_table == NULL)
-			error("can't allocate memory for SGSshift_table");
+		if (VSGSshift_table == NULL)
+			error("can't allocate memory for VSGSshift_table");
 	}
 	for (u = 0; u < 256; u++) {
 		for (j = 0; j < nP0; j++) {
 			c = (char) u;
-			SGSshift(c, j) = 0;
+			VSGSshift(c, j) = 0;
 		}
 	}
 }
 
 
 /****************************************************************************
- * The Good Right Shifts
- * =====================
+ * The Matching Window shifts
+ * ==========================
  *
  * Definition
  * ----------
  *
  * For any j1 and j2 such that 0 <= j1 < j2 <= nP (nP being the length of the
  * pattern P), we note P(j1, j2) the subpattern of P starting at j1 and
- * ending at j2-1. Then GRS(j1, j2), the "Good Right Shift" for P(j1, j2), is
- * defined by:
+ * ending at j2-1. Then MWshift(j1, j2), the "Matching Window shift" for
+ * P(j1, j2), is defined by:
  *   Imagine that, for a given alignement of P with the subject S, all
  *   letters in subpattern P(j1, j2) are matching S (we say that the current
- *   matching window is (j1, j2)). Then GRS(j1, j2) is the smallest (non-zero)
- *   amount of letters that we can shift P to the right without introducing an
- *   immediate mismatch.
+ *   Matching Window is (j1, j2)). Then MWshift(j1, j2) is the smallest
+ *   (non-zero) amount of letters that we can shift P to the right without
+ *   introducing an immediate mismatch.
  *
- * How to compute the GRS function
- * --------------------------------
+ * How to compute the MWshift function
+ * -----------------------------------
  * 
- * The GRS function depends only on the pattern and therefore can be
+ * The MWshift function depends only on the pattern and therefore can be
  * preprocessed.
- * To compute GRS(j1, j2) we need to solve a matching problem again but within
- * P itself i.e. we must find the smallest (non-zero) amount of letters that we
- * need to shift P(j1, j2) to the _left_ until it matches P again.
+ * To compute MWshift(j1, j2) we need to solve a matching problem again but
+ * within P itself i.e. we must find the smallest (non-zero) amount of letters
+ * that we need to shift P(j1, j2) to the _left_ until it matches P again.
  * IMPORTANT: This match must be a "full" match (when we are still within the
  * limits of P) and a "partial" match (when we are out of limits i.e. we've
- * moved to far to the left). For a "partial" match, all letters that are still
- * within P limits must match. When we have moved so far that all letters are
- * out of limits, then we have a "partial" match too.
- * This ensure that GRS(j1, j2) is always <= j2.
+ * moved to far to the left). For a "partial" match, all letters that are
+ * still within P limits must match. When we have moved so far that all
+ * letters are out of limits, then we have a "partial" match too.
+ * This ensure that MWshift(j1, j2) is always <= j2.
  * 
  * Example: P = acbaba
  *              012345
  * 
- *   GRS(0,1) = 1
- *   GRS(1,2) = 2
- *   GRS(2,3) = 3
- *   GRS(3.4) = GRS(2,4) = GRS(1,4) = GRS(0,4) = 3
+ *   MWshift(0,1) = 1
+ *   MWshift(1,2) = 2
+ *   MWshift(2,3) = 3
+ *   MWshift(3.4) = MWshift(2,4) = MWshift(1,4) = MWshift(0,4) = 3
  *   ...
- *   GRS(4,5) = GRS(5,6) = GRS(4,6) = 2
- *   GRS(0,6) = 5 (GRS max)
+ *   MWshift(4,5) = MWshift(5,6) = MWshift(4,6) = 2
+ *   MWshift(0,6) = 5 (MWshift max)
  * 
- * A nice property of the GRS function is that:
- *     if j1 <= j1' < j2' <= j2, then GRS(j1', j2') <= GRS(j1, j2)
- * Therefore the biggest value for GRS is reached for j1 = 0 and j2 = nP.
+ * A nice property of the MWshift function is that:
+ *     if j1 <= j1' < j2' <= j2, then MWshift(j1', j2') <= MWshift(j1, j2)
+ * Therefore the biggest value for MWshift is reached for j1 = 0 and j2 = nP.
  * To summarize:
- *     1 <= GRS(j1, j2) <= min(j2, GRS(0, nP))
- * Another property of the GRS function is that if P1 and P2 are 2 patterns
- * such that P1 is a prefix of P2 then GRS1 and GRS2 (their respective GRS
- * functions) are equal on the set of points (j1, j2) that are valid for GRS1.
- * This last property is used by the init_GRS_table() C function below.
+ *     1 <= MWshift(j1, j2) <= min(j2, MWshift(0, nP))
+ * Another property of the MWshift function is that if P1 and P2 are 2
+ * patterns such that P1 is a prefix of P2 then MWshift1 and MWshift2 (their
+ * respective MWshift functions) are equal on the set of points (j1, j2) that
+ * are valid for MWshift1.
+ * This last property is used by the init_MWshift_table() C function below.
  * 
- * No need to preprocess the GRS function
+ * No need to preprocess the MWshift function
  * --------------------------------------
  *
  * However this preprocessing would be too expensive since it requires the
@@ -222,14 +220,14 @@ static void init_SGSshift_table()
  * string matching sub-problem with a cost of its own. So the trick is to
  * delay those evaluations until they are needed, and the fact is that, in
  * practise, very few of them are actually needed compared to the total
- * number of possible GRS(j1, j2) values.
+ * number of possible MWshift(j1, j2) values.
  */
 
-/* Contains the GRS values for current pattern P0. */
-static int *GRS_table = NULL;
+/* Contains the MWshift values for current pattern P0. */
+static int *MWshift_table = NULL;
 
-/* GRS_table is a 2-dim array with nrow = ncol = P0buffer_length.
- * The layout of GRS_table is (only the values marked with an "x" will
+/* MWshift_table is a 2-dim array with nrow = ncol = P0buffer_length.
+ * The layout of MWshift_table is (only the values marked with an "x" will
  * be potentially used):
  *
  *           1 2 3 4 5 6 j2
@@ -244,65 +242,65 @@ static int *GRS_table = NULL;
  * The "x" region is defined by 0 <= j1 < j2 <= nP0
  */
 
-#define GRS(j1, j2)	(GRS_table[P0buffer_length * (j1) + (j2) - 1])
+#define MWshift(j1, j2)	(MWshift_table[P0buffer_length * (j1) + (j2) - 1])
 
-static int get_GRS(int j1, int j2)
+static int get_MWshift(int j1, int j2)
 {
-	int grs, k1, k2, length;
+	int shift, k1, k2, length;
 
-	grs = GRS(j1, j2);
-	if (grs != 0)
-		return grs;
-	for (grs = 1; grs < j2; grs++) {
-		if (grs < j1) k1 = j1 - grs; else k1 = 0;
-		k2 = j2 - grs;
+	shift = MWshift(j1, j2);
+	if (shift != 0)
+		return shift;
+	for (shift = 1; shift < j2; shift++) {
+		if (shift < j1) k1 = j1 - shift; else k1 = 0;
+		k2 = j2 - shift;
 		length = k2 - k1;
-		if (memcmp(P0buffer + k1, P0buffer + j2 - length, length) == 0)
+		if (memcmp(P0buffer + k1, P0buffer + k1 + shift, length) == 0)
 			break;
 	}
-	/* grs is j2 when the "for" loop is not interrupted by "break" */
-	return GRS(j1, j2) = grs;
+	/* shift is j2 when the "for" loop is not interrupted by "break" */
+	return MWshift(j1, j2) = shift;
 }
 
-static void init_GRS_table()
+static void init_MWshift_table()
 {
 	int j1, j2 = 1;
 
-	if (P0buffer_init_status == -1 && GRS_table != NULL) {
-		free(GRS_table);
-		GRS_table = NULL;
+	if (P0buffer_init_status == -1 && MWshift_table != NULL) {
+		free(MWshift_table);
+		MWshift_table = NULL;
 	}
-	if (P0buffer_length != 0 && GRS_table == NULL) {
-		GRS_table = (int *) malloc(P0buffer_length * P0buffer_length *
+	if (P0buffer_length != 0 && MWshift_table == NULL) {
+		MWshift_table = (int *) malloc(P0buffer_length * P0buffer_length *
 						sizeof(int));
-		if (GRS_table == NULL)
-			error("can't allocate memory for GRS_table");
+		if (MWshift_table == NULL)
+			error("can't allocate memory for MWshift_table");
 	}
 	if (P0buffer_init_status != -1)
 		j2 = P0buffer_init_status + 1;
 	for ( ; j2 <= nP0; j2++) {
 		for (j1 = 0; j1 < j2; j1++) {
-			GRS(j1, j2) = 0;
+			MWshift(j1, j2) = 0;
 		}
 	}
 }
 
 
 /****************************************************************************
- * The GRS-based exact matching algo
- * =================================
+ * The boyermoore() function
+ * =========================
  * 
  * My poor understanding of the original Boyer-Moore algo as explained in Dan
  * Gusfield book "Algorithms on strings, trees, and sequences", is that it
  * will be inefficent in this situation:
  *   S = "Xbabababababab"
  *   P = "abababababab"
- * because after it has applied the strong good suffix rule and shifted P 2
+ * because after it has applied the "strong good suffix rule" and shifted P 2
  * letters to the right, it will start comparing P and S suffixes again which
  * is a waste of time.
- * The original idea of the "GRS feature" introduced in the boyermoore()
+ * The original idea of the "MWshift feature" introduced in the boyermoore()
  * function below was to never compare twice the letters that are in the
- * current matching window (defined by (j1, j2) in P and (i1, i2) in S).
+ * current Matching Window (defined by (j1, j2) in P and (i1, i2) in S).
  */
 
 #define ADJUSTMW(i, j, shift) \
@@ -319,21 +317,21 @@ static void init_GRS_table()
 static int boyermoore(char *P, int nP, char *S, int nS, int is_count_only)
 {
 	int count = 0, n, i1, i2, j1, j2, shift0, shift1, i, j;
-	char Pmrc, c; /* Pmrc is P most right char */
+	char Pmrc, c; /* Pmrc is P right-most char */
 
 	init_P0buffer(P, nP);
-	init_SGSshift_table();
-	if (nP <= MAXNP_WITH_GRS)
-		init_GRS_table();
+	init_VSGSshift_table();
+	if (nP <= MWSHIFT_NPMAX)
+		init_MWshift_table();
 	Pmrc = P[nP-1];
 	n = nP - 1;
 	j2 = 0;
 	while (n < nS) {
 		if (j2 == 0) {
-			/* No matching window yet, we need to find one */
+			/* No Matching Window yet, we need to find one */
 			c = S[n];
 			if (c != Pmrc) {
-				shift0 = get_SGSshift(c, nP-1);
+				shift0 = get_VSGSshift(c, nP-1);
 				n += shift0;
 				continue;
 			}
@@ -341,9 +339,9 @@ static int boyermoore(char *P, int nP, char *S, int nS, int is_count_only)
 			i2 = i1 + 1;
 			j2 = nP;
 			j1 = j2 - 1;
-			/* Now we have a matching window (1-letter suffix) */
+			/* Now we have a Matching Window (1-letter suffix) */
 		}
-		/* We try to extend the current matching window... */
+		/* We try to extend the current Matching Window... */
 		if (j1 > 0) {
 			/* ... to the left */
 			for (i = i1-1, j = j1-1; j >= 0; i--, j--)
@@ -358,31 +356,31 @@ static int boyermoore(char *P, int nP, char *S, int nS, int is_count_only)
 				if (S[i2] != P[j2])
 					break;
 		}
-		if (j2 == nP) { /* matching window is a suffix */
+		if (j2 == nP) { /* the Matching Window is a suffix */
 			if (j1 == 0) {
 				/* we have a full match! */
 				if (!is_count_only)
 					Biostrings_reportMatch(i1);
 				count++;
-				shift0 = get_SGSshift(P[0], 0); /* = max(GRS) */
+				shift0 = get_VSGSshift(P[0], 0); /* = max(MWshift) */
 			} else {
-				shift0 = get_SGSshift(c, j1 - 1);
+				shift0 = get_VSGSshift(c, j1 - 1);
 			}
 		} else {
-			shift0 = get_GRS(j1, j2);
+			shift0 = get_MWshift(j1, j2);
 			c = S[n];
 			if (c != Pmrc) {
-				shift1 = get_SGSshift(c, nP-1);
+				shift1 = get_VSGSshift(c, nP-1);
 				if (shift1 > shift0)
 					shift0 = shift1;
 			}
 		}
 		n += shift0;
-		if (nP <= MAXNP_WITH_GRS) {
+		if (nP <= MWSHIFT_NPMAX) {
 			ADJUSTMW(i1, j1, shift0)
 			ADJUSTMW(i2, j2, shift0)
 		} else {
-			j2 = 0; /* forget the current matching window */
+			j2 = 0; /* forget the current Matching Window */
 		}
 	}
 	return count;
