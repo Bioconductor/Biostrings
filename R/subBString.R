3fb57e3e44fef4d3abd25b166615d4b143c06b31
Herve Pages <hpages@fhcrc.org>
2008-02-29 06:41:28 +0000

    #oops
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@30280 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/subBString.R
+++ R/subBString.R
@@ -130,7 +130,8 @@ setMethod("subviews", "BStringViews",
         if (check.limits && (any(start < 1L) || any(nchar(subject(x)) < end)))
             stop("result contains \"out of limits\" views")
         ans_width <- end - start + 1L
-        new("BStringViews", subject(x), start=start, width=ans_width, check=FALSE)
+        new("BStringViews", subject(x),
+            start=start, width=ans_width, desc=desc(x), check=FALSE)
     }
 )
 
