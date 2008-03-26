a63b14ae98d6deef7f277150b78e6f2eda6f5645
Herve Pages <hpages@fhcrc.org>
2008-03-26 21:15:17 +0000

    added the lettersToAmbiguity() utility function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@30689 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/IUPAC_CODE_MAP.R
+++ R/IUPAC_CODE_MAP.R
@@ -20,3 +20,23 @@ IUPAC_CODE_MAP <- c(
     N="ACGT"
 )
 
+lettersToAmbiguity <- function(x)
+{
+    if (!is.character(x) || any(is.na(x)) || any(nchar(x) == 0))
+        stop("'x' must be a vector of non-empty character strings")
+    if (length(x) == 0)
+        return(character(0))
+    IUPAC_code_revmap <- names(IUPAC_CODE_MAP)
+    names(IUPAC_code_revmap) <- IUPAC_CODE_MAP
+    toAmbiguity <- function(xx)
+    {
+        all_bases <- unique(unlist(strsplit(IUPAC_CODE_MAP[xx], "", fixed=TRUE)))
+        if (any(is.na(all_bases)))
+            stop("some strings in 'x' contain non IUPAC letters")
+        ans <- IUPAC_code_revmap[paste(sort(all_bases), collapse="")]
+        names(ans) <- NULL
+        ans
+    }
+    sapply(strsplit(x, "", fixed=TRUE), toAmbiguity)
+}
+
