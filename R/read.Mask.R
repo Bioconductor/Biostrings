4ac3200dac3365505e4531296b75fdc76c323f76
Herve Pages <hpages@fhcrc.org>
2008-05-14 00:16:19 +0000

    Added the read.agpMask() function for extracting a mask from an NCBI "agp" file. Version bumped to 2.9.9
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@31655 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/read.Mask.R
+++ R/read.Mask.R
@@ -2,29 +2,113 @@
 ### Read a mask from a file
 ### -----------------------
 ###
+### From an NCBI "agp" file (for chrY in hs b36v3):
+###   library(BSgenome.Hsapiens.NCBI.b36v3)
+###   file1 <- system.file("extdata", "hs_b36v3_chrY.agp", package="Biostrings")
+###   mask1 <- read.agpMask(file1, length(Hsapiens$chrY), seqname="chrY")
 ### From an UCSC "lift" file (for hg18):
-###   file1 <- system.file("extdata", "hg18liftAll.lft", package="Biostrings")
-###   mask1 <- read.liftMask(file1, "chr1")
+###   file2 <- system.file("extdata", "hg18liftAll.lft", package="Biostrings")
+###   mask2 <- read.liftMask(file2, seqname="chr1")
 ### From a RepeatMasker .out file (for chrM in ce2):
 ###   library(BSgenome.Celegans.UCSC.ce2)
-###   file2 <- system.file("extdata", "ce2chrM.fa.out", package="Biostrings")
-###   mask2 <- read.rmMask(file2, width=length(Celegans$chrM)) 
+###   file3 <- system.file("extdata", "ce2chrM.fa.out", package="Biostrings")
+###   mask3 <- read.rmMask(file3, length(Celegans$chrM)) 
 ### From a Tandem Repeats Finder .bed file (for chrM in ce2):
-###   file3 <- system.file("extdata", "ce2chrM.bed", package="Biostrings")
-###   mask3 <- read.trfMask(file3, width=length(Celegans$chrM)) 
+###   file4 <- system.file("extdata", "ce2chrM.bed", package="Biostrings")
+###   mask4 <- read.trfMask(file4, length(Celegans$chrM)) 
 ###
 ### -------------------------------------------------------------------------
 
 
-read.liftMask <- function(file, seqname=NA, width=NA)
+.emptyMaskWithWarning <- function(mask_name, seqname, width)
 {
-    if (!isSingleStringOrNA(seqname))
-        stop("'seqname' must be a single string or 'NA'")
+    warning("no ", mask_name, " for sequence \"", seqname, "\" in this file, ",
+            "returning empty mask")
+    ans <- Mask(width, start=integer(0), width=integer(0))
+    names(ans) <- paste(mask_name, "(empty)")
+    ans
+}
+
+read.agpMask <- function(file, width, seqname="?", gap.types=NULL, use.gap.types=TRUE)
+{
+    if (!isSingleNumber(width))
+        stop("'width' must be a single integer")
+    if (!is.integer(width))
+        width <- as.integer(width)
+    if (!isSingleString(seqname))
+        stop("'seqname' must be a single string")
+    if (!is.null(gap.types) && (!is.character(gap.types)
+                                || any(is.na(gap.types))
+                                || any(duplicated(gap.types))))
+        stop("'gap.types' must be 'NULL' or a character vector ",
+             "with no NAs and no duplicated")
+    if (!isTRUEorFALSE(use.gap.types))
+        stop("'use.gap.types' must be 'TRUE' or 'FALSE'")
+    ALL_COLS <- c(
+        `chrom`="character",
+        `chr_start`="integer",
+        `chr_stop`="integer",
+        `part_no`="integer",
+        `part_type`="character",
+        `gap_len`="character",
+        `gap_type`="character",
+        `linkage`="character",
+        `empty`="character"
+    )
+    COLS <- c(
+        "chrom",
+        "chr_start",
+        "chr_stop",
+        "part_type",
+        "gap_len",
+        "gap_type"
+    )
+    ALL_COLS[!(names(ALL_COLS) %in% COLS)] <- "NULL"
+    data <- read.table(file,
+                       sep="\t",
+                       col.names=names(ALL_COLS),
+                       colClasses=ALL_COLS,
+                       check.names=FALSE,
+                       fill=TRUE)
+    if (seqname == "?") {
+        found_seqnames <- paste("\"", unique(data$chrom), "\"", sep="")
+        found_seqnames <- paste(found_seqnames, collapse=", ")
+        stop("seqnames found in this file: ", found_seqnames)
+    }
+    data <- data[(data$chrom %in% seqname) & (data$part_type %in% "N"), ]
+    if (length(gap.types) == 1 && gap.types == "?") {
+        found_types <- paste("\"", unique(data$gap_type), "\"", sep="")
+        found_types <- paste(found_types, collapse=", ")
+        stop("gap types found in this file for sequence \"", seqname, "\": ", found_types)
+    }
+    mask_name <- "N-gaps"
+    if (!is.null(gap.types)) {
+        data <- data[data$gap_type %in% gap.types, ]
+        mask_name <- paste(mask_name, " [type=", paste(gap.types, collapse="|"), "]", sep="")
+    }
+    if (nrow(data) == 0)
+        return(.emptyMaskWithWarning(mask_name, seqname, width))
+    ranges <- IRanges(start=data$chr_start, width=as.integer(data$gap_len))
+    ## Sanity check
+    if (!identical(end(ranges), data$chr_stop))
+        stop("broken \"agp\" file: contains inconsistent ",
+             "chr_start/chr_stop/gap_len values ",
+             "for N-gaps in sequence \"", seqname, "\"")
+    nir1 <- asNormalIRanges(ranges)
+    if (use.gap.types)
+        names(nir1) <- data$gap_type
+    new("MaskCollection", nir_list=list(nir1), width=width, active=TRUE, NAMES=mask_name)
+}
+
+read.liftMask <- function(file, seqname="?", width=NA)
+{
+    if (!isSingleString(seqname))
+        stop("'seqname' must be a single string")
     if (!isSingleNumberOrNA(width))
         stop("'width' must be a single integer or 'NA'")
     if (!is.integer(width))
         width <- as.integer(width)
-    ALLCOLS <- c(
+    ALL_COLS <- c(
         `offset`="integer",
         `xxxx`="NULL",  # not sure how to call this
         `width`="integer",
@@ -32,10 +116,10 @@ read.liftMask <- function(file, seqname=NA, width=NA)
         `seqlen`="integer"
     )
     data <- read.table(file,
-                       col.names=names(ALLCOLS),
-                       colClasses=ALLCOLS,
+                       col.names=names(ALL_COLS),
+                       colClasses=ALL_COLS,
                        check.names=FALSE)
-    if (is.na(seqname)) {
+    if (seqname == "?") {
         found_seqnames <- paste("\"", unique(data$seqname), "\"", sep="")
         found_seqnames <- paste(found_seqnames, collapse=", ")
         stop("seqnames found in this file: ", found_seqnames)
@@ -78,7 +162,7 @@ read.rmMask <- function(file, width, use.IDs=FALSE)
         stop("'use.IDs' must be 'TRUE' or 'FALSE'")
     ## For a description of RepeatMasker output format:
     ##   http://www.repeatmasker.org/webrepeatmaskerhelp.html
-    ALLCOLS <- c(
+    ALL_COLS <- c(
         `SW_score`="integer",
         `perc_div`="numeric",
         `perc_del`="numeric",
@@ -96,10 +180,10 @@ read.rmMask <- function(file, width, use.IDs=FALSE)
         `ID`="character"
     )
     COLS <- c("begin_in_query", "end_in_query", "ID")
-    ALLCOLS[!(names(ALLCOLS) %in% COLS)] <- "NULL"
+    ALL_COLS[!(names(ALL_COLS) %in% COLS)] <- "NULL"
     data <- read.table(file,
-                       col.names=names(ALLCOLS),
-                       colClasses=ALLCOLS,
+                       col.names=names(ALL_COLS),
+                       colClasses=ALL_COLS,
                        skip=3,
                        check.names=FALSE)
     ranges <- IRanges(start=data$begin_in_query, end=data$end_in_query)
@@ -122,7 +206,7 @@ read.trfMask <- function(file, width)
         stop("'width' must be a single integer")
     if (!is.integer(width))
         width <- as.integer(width)
-    ALLCOLS <- c(
+    ALL_COLS <- c(
         `chrom`="character",
         `chromStart`="integer",
         `chromEnd`="integer",
@@ -141,10 +225,10 @@ read.trfMask <- function(file, width)
         `sequence`="character"
     )
     COLS <- c("chromStart", "chromEnd")
-    ALLCOLS[!(names(ALLCOLS) %in% COLS)] <- "NULL"
+    ALL_COLS[!(names(ALL_COLS) %in% COLS)] <- "NULL"
     data <- read.table(file,
-                       col.names=names(ALLCOLS),
-                       colClasses=ALLCOLS,
+                       col.names=names(ALL_COLS),
+                       colClasses=ALL_COLS,
                        check.names=FALSE)
     ranges <- IRanges(start=data$chromStart+1, end=data$chromEnd)
     nir1 <- toNormalIRanges(ranges)
