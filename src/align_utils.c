69672a91596cd2dc60cadab28dd9d993508a1801
Patrick Aboyoun <unknown>
2008-07-14 17:22:18 +0000

    Minor speedup enhancements to alignment utility functions.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@32694 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/align_utils.c
+++ src/align_utils.c
@@ -4,20 +4,21 @@
 SEXP AlignedXStringSet_nchar(SEXP alignedXStringSet)
 {
 	SEXP range = GET_SLOT(alignedXStringSet, install("range"));
-	int *rangeWidth = INTEGER(_get_IRanges_width(range));
 	SEXP indel = GET_SLOT(alignedXStringSet, install("indel"));
 	int numberOfAlignments = LENGTH(indel);
 
 	SEXP output;
 	PROTECT(output = NEW_INTEGER(numberOfAlignments));
-	int *outputPtr = INTEGER(output);
-	for (int i = 0; i < numberOfAlignments; i++) {
+	int i, j, *outputPtr;
+	const int *rangeWidth, *indelWidth;
+	for (i = 0, rangeWidth = INTEGER(_get_IRanges_width(range)), outputPtr = INTEGER(output);
+			i < numberOfAlignments; i++, rangeWidth++, outputPtr++) {
 		SEXP indelElement = VECTOR_ELT(indel, i);
-		int numberOfIndel = LENGTH(_get_IRanges_width(indelElement));
-		int *indelWidth = INTEGER(_get_IRanges_width(indelElement));
-		outputPtr[i] = rangeWidth[i];
-		for (int j = 0; j < numberOfIndel; j++) {
-			outputPtr[i] += indelWidth[j];
+		int numberOfIndels = LENGTH(_get_IRanges_width(indelElement));
+		*outputPtr = *rangeWidth;
+		for (j = 0, indelWidth = INTEGER(_get_IRanges_width(indelElement));
+		     j < numberOfIndels; j++, indelWidth++) {
+			*outputPtr += *indelWidth;
 		}
 	}
 	UNPROTECT(1);
@@ -28,13 +29,12 @@ SEXP AlignedXStringSet_nchar(SEXP alignedXStringSet)
 
 SEXP AlignedXStringSet_align_aligned(SEXP alignedXStringSet, SEXP gapCode)
 {
+	int i, j;
 	char gapCodeValue = (char) RAW(gapCode)[0];
 
 	SEXP unaligned = GET_SLOT(alignedXStringSet, install("unaligned"));
 	CachedXStringSet cachedAlignedXStringSet = _new_CachedXStringSet(unaligned);
 	SEXP range = GET_SLOT(alignedXStringSet, install("range"));
-	int *rangeStart = INTEGER(_get_IRanges_start(range));
-	int *rangeWidth = INTEGER(_get_IRanges_width(range));
 	SEXP indel = GET_SLOT(alignedXStringSet, install("indel"));
 
 	const char *stringSetClass = _get_class(unaligned);
@@ -50,13 +50,17 @@ SEXP AlignedXStringSet_align_aligned(SEXP alignedXStringSet, SEXP gapCode)
 	PROTECT(alignedWidth = AlignedXStringSet_nchar(alignedXStringSet));
 	PROTECT(alignedStart = NEW_INTEGER(LENGTH(alignedWidth)));
 	int totalNChars = 0;
-	for (int i = 0; i < LENGTH(alignedWidth); i++) {
-		totalNChars += INTEGER(alignedWidth)[i];
+	const int *width_i, *start_i;
+	int *start_iPlus1; 
+	for (i = 0, width_i = INTEGER(alignedWidth); i < LENGTH(alignedWidth); i++, width_i++) {
+		totalNChars += *width_i;
 	}
 	if (totalNChars > 0) {
 		INTEGER(alignedStart)[0] = 1;
-		for (int i = 0; i < LENGTH(alignedWidth) - 1; i++) {
-			INTEGER(alignedStart)[i+1] = INTEGER(alignedStart)[i] + INTEGER(alignedWidth)[i];
+		for (i = 0, start_i = INTEGER(alignedStart), width_i = INTEGER(alignedWidth),
+				start_iPlus1 = INTEGER(alignedStart) + 1; i < LENGTH(alignedWidth) - 1;
+				i++, start_i++, width_i++, start_iPlus1++) {
+			*start_iPlus1 = *start_i + *width_i;
 		}
 	}
 	SEXP alignedStringTag, alignedStringXData;
@@ -70,21 +74,24 @@ SEXP AlignedXStringSet_align_aligned(SEXP alignedXStringSet, SEXP gapCode)
 
 	int stringIncrement = (numberOfStrings == 1 ? 0 : 1);
 	int index = 0, stringElement = 0;
-	for (int i = 0; i < numberOfAlignments; i++) {
+	const int *rangeStart, *rangeWidth;
+	for (i = 0, rangeStart = INTEGER(_get_IRanges_start(range)), rangeWidth = INTEGER(_get_IRanges_width(range));
+	         i < numberOfAlignments; i++, rangeStart++, rangeWidth++) {
 		RoSeq origString = _get_CachedXStringSet_elt_asRoSeq(&cachedAlignedXStringSet, stringElement);
-		char *origStringPtr = (char *) (origString.elts + (rangeStart[i] - 1));
+		char *origStringPtr = (char *) (origString.elts + (*rangeStart - 1));
 		SEXP indelElement = VECTOR_ELT(indel, i);
 		int numberOfIndel = LENGTH(_get_IRanges_start(indelElement));
-		int *indelStart = INTEGER(_get_IRanges_start(indelElement));
-		int *indelWidth = INTEGER(_get_IRanges_width(indelElement));
 		if (numberOfIndel == 0) {
-			memcpy(&alignedStringPtr[index], origStringPtr, rangeWidth[i] * sizeof(char));
-			index += rangeWidth[i];
+			memcpy(&alignedStringPtr[index], origStringPtr, *rangeWidth * sizeof(char));
+			index += *rangeWidth;
 		} else {
 			int prevStart = 0;
-			for (int j = 0; j < numberOfIndel; j++) {
-				int currStart = indelStart[j] - 1;
-				int currWidth = indelWidth[j];
+			const int *indelStart, *indelWidth;
+			for (j = 0, indelStart = INTEGER(_get_IRanges_start(indelElement)),
+					indelWidth = INTEGER(_get_IRanges_width(indelElement));
+			        j < numberOfIndel; j++, indelStart++, indelWidth++) {
+				int currStart = *indelStart - 1;
+				int currWidth = *indelWidth;
 				int copyElements = currStart - prevStart;
 				if (copyElements > 0) {
 					memcpy(&alignedStringPtr[index], origStringPtr, copyElements * sizeof(char));
@@ -97,7 +104,7 @@ SEXP AlignedXStringSet_align_aligned(SEXP alignedXStringSet, SEXP gapCode)
 				}
 				prevStart = currStart;
 			}
-			int copyElements = rangeWidth[i] - prevStart;
+			int copyElements = *rangeWidth - prevStart;
 			memcpy(&alignedStringPtr[index], origStringPtr, copyElements * sizeof(char));
 			index += copyElements;
 		}
@@ -119,18 +126,22 @@ SEXP align_compareStrings(SEXP patternStrings, SEXP subjectStrings, SEXP maxNCha
 	char *outputPtr = (char *) R_alloc((long) (INTEGER(maxNChar)[0] + 1), sizeof(char));
 	SEXP output;
 	PROTECT(output = NEW_CHARACTER(numberOfStrings));
-	for (int i = 0; i < numberOfStrings; i++) {
-		char *patternPtr = (char *) CHAR(STRING_ELT(patternStrings, i));
-		char *subjectPtr = (char *) CHAR(STRING_ELT(subjectStrings, i));
+	int i, j;
+	char *output_j;
+	const char *subject_j;
+	for (i = 0; i < numberOfStrings; i++) {
+		const char *patternPtr = (char *) CHAR(STRING_ELT(patternStrings, i));
+		const char *subjectPtr = (char *) CHAR(STRING_ELT(subjectStrings, i));
 		int numberOfChars = strlen(patternPtr);
 		memcpy(outputPtr, patternPtr, numberOfChars * sizeof(char));
 		outputPtr[numberOfChars] = '\0';
-		for (int j = 0; j < numberOfChars; j++) {
-			if (outputPtr[j] != deletionChar) {
-				if (subjectPtr[j] == deletionChar) {
-					outputPtr[j] = insertionChar;
-				} else if (subjectPtr[j] != outputPtr[j]) {
-					outputPtr[j] = mismatchChar;
+		for (j = 0, output_j = outputPtr, subject_j = subjectPtr;
+		     j < numberOfChars; j++, output_j++, subject_j++) {
+			if (*output_j != deletionChar) {
+				if (*subject_j == deletionChar) {
+					*output_j = insertionChar;
+				} else if (*subject_j != *output_j) {
+					*output_j = mismatchChar;
 				}
 			}
 		}
