d582818f2e909245eeacead13a8c8ce474c64101
Herve Pages <hpages@fhcrc.org>
2008-08-10 03:05:25 +0000

    [code cleaning] Use of macros in C code in charge of .Call entry points and C callable registration. This makes the code cleaner, shorter, and easier to read/maintain.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@33152 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/R_init_Biostrings.c
+++ src/R_init_Biostrings.c
@@ -1,204 +1,209 @@
 #include "Biostrings.h"
 
+#define CALLMETHOD_DEF(fun, numArgs) {#fun, (DL_FUNC) &fun, numArgs}
+
+#define REGISTER_CCALLABLE(fun) \
+	R_RegisterCCallable("Biostrings", #fun, (DL_FUNC) &fun)
+
 static const R_CallMethodDef callMethods[] = {
 
 /* copy_seq.c */
-	{"debug_copy_seq", (DL_FUNC) &debug_copy_seq, 0},
+	CALLMETHOD_DEF(debug_copy_seq, 0),
 
 /* utils.c */
-	{"debug_utils", (DL_FUNC) &debug_utils, 0},
-	{"Biostrings_length_vectors_in_list", (DL_FUNC) &Biostrings_length_vectors_in_list, 1},
+	CALLMETHOD_DEF(debug_utils, 0),
+	CALLMETHOD_DEF(Biostrings_length_vectors_in_list, 1),
 
 /* RoSeq_utils.c */
-	{"debug_RoSeq_utils", (DL_FUNC) &debug_RoSeq_utils, 0},
+	CALLMETHOD_DEF(debug_RoSeq_utils, 0),
 
 /* SparseList_utils.c */
-	{"debug_SparseList_utils", (DL_FUNC) &debug_SparseList_utils, 0},
+	CALLMETHOD_DEF(debug_SparseList_utils, 0),
 
 /* XRaw_class.c */
-	{"debug_XRaw_class", (DL_FUNC) &debug_XRaw_class, 0},
+	CALLMETHOD_DEF(debug_XRaw_class, 0),
 
-	{"Biostrings_sexp_address", (DL_FUNC) &Biostrings_sexp_address, 1},
-	{"Biostrings_safe_explode", (DL_FUNC) &Biostrings_safe_explode, 1},
-	{"Biostrings_xp_show", (DL_FUNC) &Biostrings_xp_show, 1},
-	{"Biostrings_xp_new", (DL_FUNC) &Biostrings_xp_new, 0},
-	{"Biostrings_XRaw_alloc", (DL_FUNC) &Biostrings_XRaw_alloc, 2},
-	{"Biostrings_XRaw_get_show_string", (DL_FUNC) &Biostrings_XRaw_get_show_string, 1},
-	{"Biostrings_XRaw_length", (DL_FUNC) &Biostrings_XRaw_length, 1},
+	CALLMETHOD_DEF(Biostrings_sexp_address, 1),
+	CALLMETHOD_DEF(Biostrings_safe_explode, 1),
+	CALLMETHOD_DEF(Biostrings_xp_show, 1),
+	CALLMETHOD_DEF(Biostrings_xp_new, 0),
+	CALLMETHOD_DEF(Biostrings_XRaw_alloc, 2),
+	CALLMETHOD_DEF(Biostrings_XRaw_get_show_string, 1),
+	CALLMETHOD_DEF(Biostrings_XRaw_length, 1),
 
 /* XRaw_utils.c */
-	{"debug_XRaw_utils", (DL_FUNC) &debug_XRaw_utils, 0},
+	CALLMETHOD_DEF(debug_XRaw_utils, 0),
 
-	{"Biostrings_XRaw_memcmp", (DL_FUNC) &Biostrings_XRaw_memcmp, 5},
+	CALLMETHOD_DEF(Biostrings_XRaw_memcmp, 5),
 
-	{"Biostrings_XRaw_copy_from_i1i2", (DL_FUNC) &Biostrings_XRaw_copy_from_i1i2, 4},
-	{"Biostrings_XRaw_copy_from_subset", (DL_FUNC) &Biostrings_XRaw_copy_from_subset, 3},
+	CALLMETHOD_DEF(Biostrings_XRaw_copy_from_i1i2, 4),
+	CALLMETHOD_DEF(Biostrings_XRaw_copy_from_subset, 3),
 
-	{"Biostrings_XRaw_read_chars_from_i1i2", (DL_FUNC) &Biostrings_XRaw_read_chars_from_i1i2, 3},
-	{"Biostrings_XRaw_read_chars_from_subset", (DL_FUNC) &Biostrings_XRaw_read_chars_from_subset, 2},
-	{"Biostrings_XRaw_write_chars_to_i1i2", (DL_FUNC) &Biostrings_XRaw_write_chars_to_i1i2, 4},
-	{"Biostrings_XRaw_write_chars_to_subset", (DL_FUNC) &Biostrings_XRaw_write_chars_to_subset, 3},
+	CALLMETHOD_DEF(Biostrings_XRaw_read_chars_from_i1i2, 3),
+	CALLMETHOD_DEF(Biostrings_XRaw_read_chars_from_subset, 2),
+	CALLMETHOD_DEF(Biostrings_XRaw_write_chars_to_i1i2, 4),
+	CALLMETHOD_DEF(Biostrings_XRaw_write_chars_to_subset, 3),
 
-	{"XRaw_read_ints_from_i1i2", (DL_FUNC) &XRaw_read_ints_from_i1i2, 3},
-	{"XRaw_read_ints_from_subset", (DL_FUNC) &XRaw_read_ints_from_subset, 2},
-	{"XRaw_write_ints_to_i1i2", (DL_FUNC) &XRaw_write_ints_to_i1i2, 4},
-	{"XRaw_write_ints_to_subset", (DL_FUNC) &XRaw_write_ints_to_subset, 3},
+	CALLMETHOD_DEF(XRaw_read_ints_from_i1i2, 3),
+	CALLMETHOD_DEF(XRaw_read_ints_from_subset, 2),
+	CALLMETHOD_DEF(XRaw_write_ints_to_i1i2, 4),
+	CALLMETHOD_DEF(XRaw_write_ints_to_subset, 3),
 
-	{"XRaw_read_enc_chars_from_i1i2", (DL_FUNC) &XRaw_read_enc_chars_from_i1i2, 4},
-	{"XRaw_read_enc_chars_from_subset", (DL_FUNC) &XRaw_read_enc_chars_from_subset, 3},
-	{"XRaw_write_enc_chars_to_i1i2", (DL_FUNC) &XRaw_write_enc_chars_to_i1i2, 5},
-	{"XRaw_write_enc_chars_to_subset", (DL_FUNC) &XRaw_write_enc_chars_to_subset, 4},
+	CALLMETHOD_DEF(XRaw_read_enc_chars_from_i1i2, 4),
+	CALLMETHOD_DEF(XRaw_read_enc_chars_from_subset, 3),
+	CALLMETHOD_DEF(XRaw_write_enc_chars_to_i1i2, 5),
+	CALLMETHOD_DEF(XRaw_write_enc_chars_to_subset, 4),
 
-	{"XRaw_read_complexes_from_i1i2", (DL_FUNC) &XRaw_read_complexes_from_i1i2, 4},
-	{"XRaw_read_complexes_from_subset", (DL_FUNC) &XRaw_read_complexes_from_subset, 3},
+	CALLMETHOD_DEF(XRaw_read_complexes_from_i1i2, 4),
+	CALLMETHOD_DEF(XRaw_read_complexes_from_subset, 3),
 
-	{"XRaw_loadFASTA", (DL_FUNC) &XRaw_loadFASTA, 4},
+	CALLMETHOD_DEF(XRaw_loadFASTA, 4),
 
 /* XInteger.c */
-	{"debug_XInteger", (DL_FUNC) &debug_XInteger, 0},
+	CALLMETHOD_DEF(debug_XInteger, 0),
 
-	{"XInteger_alloc", (DL_FUNC) &XInteger_alloc, 2},
-	{"XInteger_get_show_string", (DL_FUNC) &XInteger_get_show_string, 1},
-	{"XInteger_length", (DL_FUNC) &XInteger_length, 1},
-	{"XInteger_memcmp", (DL_FUNC) &XInteger_memcmp, 5},
+	CALLMETHOD_DEF(XInteger_alloc, 2),
+	CALLMETHOD_DEF(XInteger_get_show_string, 1),
+	CALLMETHOD_DEF(XInteger_length, 1),
+	CALLMETHOD_DEF(XInteger_memcmp, 5),
 
-	{"XInteger_read_ints_from_i1i2", (DL_FUNC) &XInteger_read_ints_from_i1i2, 3},
-	{"XInteger_read_ints_from_subset", (DL_FUNC) &XInteger_read_ints_from_subset, 2},
-	{"XInteger_write_ints_to_i1i2", (DL_FUNC) &XInteger_write_ints_to_i1i2, 4},
-	{"XInteger_write_ints_to_subset", (DL_FUNC) &XInteger_write_ints_to_subset, 3},
+	CALLMETHOD_DEF(XInteger_read_ints_from_i1i2, 3),
+	CALLMETHOD_DEF(XInteger_read_ints_from_subset, 2),
+	CALLMETHOD_DEF(XInteger_write_ints_to_i1i2, 4),
+	CALLMETHOD_DEF(XInteger_write_ints_to_subset, 3),
 
 /* XNumeric.c */
-	{"debug_XNumeric", (DL_FUNC) &debug_XNumeric, 0},
+	CALLMETHOD_DEF(debug_XNumeric, 0),
 
-	{"XNumeric_alloc", (DL_FUNC) &XNumeric_alloc, 2},
-	{"XNumeric_get_show_string", (DL_FUNC) &XNumeric_get_show_string, 1},
-	{"XNumeric_length", (DL_FUNC) &XNumeric_length, 1},
-	{"XNumeric_memcmp", (DL_FUNC) &XNumeric_memcmp, 5},
+	CALLMETHOD_DEF(XNumeric_alloc, 2),
+	CALLMETHOD_DEF(XNumeric_get_show_string, 1),
+	CALLMETHOD_DEF(XNumeric_length, 1),
+	CALLMETHOD_DEF(XNumeric_memcmp, 5),
 
-	{"XNumeric_read_nums_from_i1i2", (DL_FUNC) &XNumeric_read_nums_from_i1i2, 3},
-	{"XNumeric_read_nums_from_subset", (DL_FUNC) &XNumeric_read_nums_from_subset, 2},
-	{"XNumeric_write_nums_to_i1i2", (DL_FUNC) &XNumeric_write_nums_to_i1i2, 4},
-	{"XNumeric_write_nums_to_subset", (DL_FUNC) &XNumeric_write_nums_to_subset, 3},
+	CALLMETHOD_DEF(XNumeric_read_nums_from_i1i2, 3),
+	CALLMETHOD_DEF(XNumeric_read_nums_from_subset, 2),
+	CALLMETHOD_DEF(XNumeric_write_nums_to_i1i2, 4),
+	CALLMETHOD_DEF(XNumeric_write_nums_to_subset, 3),
 
 /* XString_class.c */
-	{"debug_XString_class", (DL_FUNC) &debug_XString_class, 0},
-	{"init_DNAlkups", (DL_FUNC) &init_DNAlkups, 2},
-	{"init_RNAlkups", (DL_FUNC) &init_RNAlkups, 2},
+	CALLMETHOD_DEF(debug_XString_class, 0),
+	CALLMETHOD_DEF(init_DNAlkups, 2),
+	CALLMETHOD_DEF(init_RNAlkups, 2),
 
 /* XStringSet_class.c */
-	{"debug_XStringSet_class", (DL_FUNC) &debug_XStringSet_class, 0},
-	{"XStringSet_as_STRSXP", (DL_FUNC) &XStringSet_as_STRSXP, 2},
-	{"XStrings_to_nchars", (DL_FUNC) &XStrings_to_nchars, 1},
+	CALLMETHOD_DEF(debug_XStringSet_class, 0),
+	CALLMETHOD_DEF(XStringSet_as_STRSXP, 2),
+	CALLMETHOD_DEF(XStrings_to_nchars, 1),
 
 /* seqs_to_seqs.c */
-	{"debug_seqs_to_seqs", (DL_FUNC) &debug_seqs_to_seqs, 0},
+	CALLMETHOD_DEF(debug_seqs_to_seqs,0),
 
-	{"copy_subXRaw", (DL_FUNC) &copy_subXRaw, 4},
-	{"new_XRaw_from_STRSXP", (DL_FUNC) &new_XRaw_from_STRSXP, 5},
-	{"new_XRaw_from_XString", (DL_FUNC) &new_XRaw_from_XString, 4},
-	{"new_XStringList_from_XRaw", (DL_FUNC) &new_XStringList_from_XRaw, 4},
-	{"narrow_XStringList", (DL_FUNC) &narrow_XStringList, 4},
+	CALLMETHOD_DEF(copy_subXRaw, 4),
+	CALLMETHOD_DEF(new_XRaw_from_STRSXP, 5),
+	CALLMETHOD_DEF(new_XRaw_from_XString, 4),
+	CALLMETHOD_DEF(new_XStringList_from_XRaw, 4),
+	CALLMETHOD_DEF(narrow_XStringList, 4),
         
 /* char_frequency.c */
-	{"XString_char_frequency", (DL_FUNC) &XString_char_frequency, 3},
-	{"XStringSet_char_frequency", (DL_FUNC) &XStringSet_char_frequency, 4},
-	{"oligonucleotide_frequency", (DL_FUNC) &oligonucleotide_frequency, 4},
+	CALLMETHOD_DEF(XString_char_frequency, 3),
+	CALLMETHOD_DEF(XStringSet_char_frequency, 4),
+	CALLMETHOD_DEF(oligonucleotide_frequency, 4),
 
 /* char_translate.c */
-        {"XRaw_translate_copy_from_i1i2", (DL_FUNC) &XRaw_translate_copy_from_i1i2, 5},
-        {"XRaw_translate_copy_from_subset", (DL_FUNC) &XRaw_translate_copy_from_subset, 4},
-        {"XRaw_reverse_copy_from_i1i2", (DL_FUNC) &XRaw_reverse_copy_from_i1i2, 4},
-        {"XRaw_reverse_translate_copy_from_i1i2", (DL_FUNC) &XRaw_reverse_translate_copy_from_i1i2, 5},
-        {"XStringSet_char_translate", (DL_FUNC) &XStringSet_char_translate, 3},
+	CALLMETHOD_DEF(XRaw_translate_copy_from_i1i2, 5),
+	CALLMETHOD_DEF(XRaw_translate_copy_from_subset, 4),
+	CALLMETHOD_DEF(XRaw_reverse_copy_from_i1i2, 4),
+	CALLMETHOD_DEF(XRaw_reverse_translate_copy_from_i1i2, 5),
+	CALLMETHOD_DEF(XStringSet_char_translate, 3),
 
 /* replace_locs.c */
-	{"XString_replace_locs_bySTRSXP", (DL_FUNC) &XString_replace_locs_bySTRSXP, 6},
-	{"XString_inplace_replace_locs_bySTRSXP", (DL_FUNC) &XString_inplace_replace_locs_bySTRSXP, 4},
+	CALLMETHOD_DEF(XString_replace_locs_bySTRSXP, 6),
+	CALLMETHOD_DEF(XString_inplace_replace_locs_bySTRSXP, 4),
 
 /* inject_code.c */
-	{"inject_code", (DL_FUNC) &inject_code, 4},
+	CALLMETHOD_DEF(inject_code, 4),
 
 /* match_utils.c */
-	{"debug_match_utils", (DL_FUNC) &debug_match_utils, 0},
-	{"nmismatch_at", (DL_FUNC) &nmismatch_at, 5},
-	{"is_matching", (DL_FUNC) &is_matching, 5},
-	{"nmatch_PairwiseAlignment", (DL_FUNC) &nmatch_PairwiseAlignment, 4},
+	CALLMETHOD_DEF(debug_match_utils, 0),
+	CALLMETHOD_DEF(nmismatch_at, 5),
+	CALLMETHOD_DEF(is_matching, 5),
+	CALLMETHOD_DEF(nmatch_PairwiseAlignment, 4),
 
 /* match_reporting.c */
-	{"debug_match_reporting", (DL_FUNC) &debug_match_reporting, 0},
+	CALLMETHOD_DEF(debug_match_reporting, 0),
 
 /* match_pattern_boyermoore.c */
-	{"debug_match_pattern_boyermoore", (DL_FUNC) &debug_match_pattern_boyermoore, 0},
+	CALLMETHOD_DEF(debug_match_pattern_boyermoore, 0),
 
 /* match_pattern_shiftor.c */
-	{"debug_match_pattern_shiftor", (DL_FUNC) &debug_match_pattern_shiftor, 0},
-	{"bits_per_long", (DL_FUNC) &bits_per_long, 0},
+	CALLMETHOD_DEF(debug_match_pattern_shiftor, 0),
+	CALLMETHOD_DEF(bits_per_long, 0),
 
 /* match_pattern.c */
-	{"debug_match_pattern", (DL_FUNC) &debug_match_pattern, 0},
-	{"XString_match_pattern", (DL_FUNC) &XString_match_pattern, 6},
-	{"XStringViews_match_pattern", (DL_FUNC) &XStringViews_match_pattern, 8},
-	{"XStringSet_vmatch_pattern", (DL_FUNC) &XStringSet_vmatch_pattern, 6},
+	CALLMETHOD_DEF(debug_match_pattern, 0),
+	CALLMETHOD_DEF(XString_match_pattern, 6),
+	CALLMETHOD_DEF(XStringViews_match_pattern, 8),
+	CALLMETHOD_DEF(XStringSet_vmatch_pattern, 6),
 
 /* match_BOC.c */
-	{"debug_match_BOC", (DL_FUNC) &debug_match_BOC, 0},
-	{"match_BOC_preprocess", (DL_FUNC) &match_BOC_preprocess, 12},
-	{"match_BOC_exact", (DL_FUNC) &match_BOC_exact, 16},
+	CALLMETHOD_DEF(debug_match_BOC, 0),
+	CALLMETHOD_DEF(match_BOC_preprocess, 12),
+	CALLMETHOD_DEF(match_BOC_exact, 16),
 
 /* match_BOC2.c */
-	{"debug_match_BOC2", (DL_FUNC) &debug_match_BOC2, 0},
-	{"match_BOC2_preprocess", (DL_FUNC) &match_BOC2_preprocess, 9},
-	{"match_BOC2_exact", (DL_FUNC) &match_BOC2_exact, 13},
+	CALLMETHOD_DEF(debug_match_BOC2, 0),
+	CALLMETHOD_DEF(match_BOC2_preprocess, 9),
+	CALLMETHOD_DEF(match_BOC2_exact, 13),
 
 /* match_PWM.c */
-	{"PWM_score", (DL_FUNC) &PWM_score, 3},
-	{"match_PWM", (DL_FUNC) &match_PWM, 4},
+	CALLMETHOD_DEF(PWM_score, 3),
+	CALLMETHOD_DEF(match_PWM, 4),
 
 /* find_palindromes.c */
-	{"debug_find_palindromes", (DL_FUNC) &debug_find_palindromes, 0},
-	{"find_palindromes", (DL_FUNC) &find_palindromes, 6},
+	CALLMETHOD_DEF(debug_find_palindromes, 0),
+	CALLMETHOD_DEF(find_palindromes, 6),
 
 /* PDict_utils.c */
-	{"debug_PDict_utils", (DL_FUNC) &debug_PDict_utils, 0},
-	{"Dups_diff", (DL_FUNC) &Dups_diff, 2},
+	CALLMETHOD_DEF(debug_PDict_utils, 0),
+	CALLMETHOD_DEF(Dups_diff, 2),
 
 /* MIndex_utils.c */
-	{"debug_MIndex_utils", (DL_FUNC) &debug_MIndex_utils, 0},
-	{"shiftListOfInts", (DL_FUNC) &shiftListOfInts, 2},
-	{"extract_endIndex", (DL_FUNC) &extract_endIndex, 4},
-	{"ByPos_MIndex_coverage", (DL_FUNC) &ByPos_MIndex_coverage, 4},
-	{"ByName_MIndex_coverage", (DL_FUNC) &ByName_MIndex_coverage, 4},
-	{"ByPos_MIndex_combine", (DL_FUNC) &ByPos_MIndex_combine, 1},
+	CALLMETHOD_DEF(debug_MIndex_utils, 0),
+	CALLMETHOD_DEF(shiftListOfInts, 2),
+	CALLMETHOD_DEF(extract_endIndex, 4),
+	CALLMETHOD_DEF(ByPos_MIndex_coverage, 4),
+	CALLMETHOD_DEF(ByName_MIndex_coverage, 4),
+	CALLMETHOD_DEF(ByPos_MIndex_combine, 1),
 
 /* match_pdict_Twobit.c */
-	{"debug_match_pdict_Twobit", (DL_FUNC) &debug_match_pdict_Twobit, 0},
-	{"build_Twobit", (DL_FUNC) &build_Twobit, 2},
+	CALLMETHOD_DEF(debug_match_pdict_Twobit, 0),
+	CALLMETHOD_DEF(build_Twobit, 2),
 
 /* match_pdict_ACtree.c */
-	{"debug_match_pdict_ACtree", (DL_FUNC) &debug_match_pdict_ACtree, 0},
-	{"free_actree_nodes_buf", (DL_FUNC) &free_actree_nodes_buf, 0},
-	{"build_ACtree", (DL_FUNC) &build_ACtree, 2},
+	CALLMETHOD_DEF(debug_match_pdict_ACtree, 0),
+	CALLMETHOD_DEF(free_actree_nodes_buf, 0),
+	CALLMETHOD_DEF(build_ACtree, 2),
 
 /* match_pdict.c */
-	{"debug_match_pdict", (DL_FUNC) &debug_match_pdict, 0},
-	{"XString_match_pdict", (DL_FUNC) &XString_match_pdict, 12},
-	{"XStringViews_match_pdict", (DL_FUNC) &XStringViews_match_pdict, 14},
+	CALLMETHOD_DEF(debug_match_pdict, 0),
+	CALLMETHOD_DEF(XString_match_pdict, 12),
+	CALLMETHOD_DEF(XStringViews_match_pdict, 14),
 
 /* pmatchPattern.c */
-	{"lcprefix", (DL_FUNC) &lcprefix, 6},
-	{"lcsuffix", (DL_FUNC) &lcsuffix, 6},
+	CALLMETHOD_DEF(lcprefix, 6),
+	CALLMETHOD_DEF(lcsuffix, 6),
 
 /* align_needwunsQS.c */
-	{"align_needwunsQS", (DL_FUNC) &align_needwunsQS, 7},
+	CALLMETHOD_DEF(align_needwunsQS, 7),
 
 /* align_pairwiseAlignment.c */
-	{"XStringSet_align_pairwiseAlignment", (DL_FUNC) &XStringSet_align_pairwiseAlignment, 18},
-	{"XStringSet_align_distance", (DL_FUNC) &XStringSet_align_distance, 14},
+	CALLMETHOD_DEF(XStringSet_align_pairwiseAlignment, 18),
+	CALLMETHOD_DEF(XStringSet_align_distance, 14),
 
 /* align_utils.c */
-	{"AlignedXStringSet_nchar", (DL_FUNC) &AlignedXStringSet_nchar, 1},
-	{"AlignedXStringSet_align_aligned", (DL_FUNC) &AlignedXStringSet_align_aligned, 2},
-	{"align_compareStrings", (DL_FUNC) &align_compareStrings, 6},
+	CALLMETHOD_DEF(AlignedXStringSet_nchar, 1),
+	CALLMETHOD_DEF(AlignedXStringSet_align_aligned, 2),
+	CALLMETHOD_DEF(align_compareStrings, 6),
 
 	{NULL, NULL, 0}
 };
@@ -209,26 +214,40 @@ void R_init_Biostrings(DllInfo *info)
 	if (sizeof(Rbyte) != sizeof(char))
 		error("sizeof(Rbyte) != sizeof(char)");
 	R_registerRoutines(info, NULL, callMethods, NULL, NULL);
-	R_RegisterCCallable("Biostrings", "_new_IRanges_from_RoSeqs", (DL_FUNC) &_new_IRanges_from_RoSeqs);
-	R_RegisterCCallable("Biostrings", "_DNAencode", (DL_FUNC) &_DNAencode);
-	R_RegisterCCallable("Biostrings", "_DNAdecode", (DL_FUNC) &_DNAdecode);
-	R_RegisterCCallable("Biostrings", "_RNAencode", (DL_FUNC) &_RNAencode);
-	R_RegisterCCallable("Biostrings", "_RNAdecode", (DL_FUNC) &_RNAdecode);
-	R_RegisterCCallable("Biostrings", "_get_XString_asRoSeq", (DL_FUNC) &_get_XString_asRoSeq);
-	R_RegisterCCallable("Biostrings", "_get_XStringSet_baseClass", (DL_FUNC) &_get_XStringSet_baseClass);
-	R_RegisterCCallable("Biostrings", "_get_XStringSet_length", (DL_FUNC) &_get_XStringSet_length);
-	R_RegisterCCallable("Biostrings", "_new_CachedXStringSet", (DL_FUNC) &_new_CachedXStringSet);
-	R_RegisterCCallable("Biostrings", "_get_CachedXStringSet_elt_asRoSeq", (DL_FUNC) &_get_CachedXStringSet_elt_asRoSeq);
-	R_RegisterCCallable("Biostrings", "_get_XStringSet_elt_asRoSeq", (DL_FUNC) &_get_XStringSet_elt_asRoSeq);
-	R_RegisterCCallable("Biostrings", "_new_XStringSet_from_RoSeqs", (DL_FUNC) &_new_XStringSet_from_RoSeqs);
-	R_RegisterCCallable("Biostrings", "_set_XStringSet_names", (DL_FUNC) &_set_XStringSet_names);
-	R_RegisterCCallable("Biostrings", "_alloc_XStringSet", (DL_FUNC) &_alloc_XStringSet);
-	R_RegisterCCallable("Biostrings", "_write_RoSeq_to_CachedXStringSet_elt", (DL_FUNC) &_write_RoSeq_to_CachedXStringSet_elt);
-	R_RegisterCCallable("Biostrings", "_write_RoSeq_to_XStringSet_elt", (DL_FUNC) &_write_RoSeq_to_XStringSet_elt);
-	R_RegisterCCallable("Biostrings", "_new_RoSeqs_from_CharAEAE", (DL_FUNC) &_new_RoSeqs_from_CharAEAE);
-	R_RegisterCCallable("Biostrings", "_new_STRSXP_from_RoSeqs", (DL_FUNC) &_new_STRSXP_from_RoSeqs);
-	R_RegisterCCallable("Biostrings", "_init_match_reporting", (DL_FUNC) &_init_match_reporting);
-	R_RegisterCCallable("Biostrings", "_report_match", (DL_FUNC) &_report_match);
-	R_RegisterCCallable("Biostrings", "_reported_matches_asSEXP", (DL_FUNC) &_reported_matches_asSEXP);
+
+/* RoSeq_utils.c */
+	REGISTER_CCALLABLE(_new_IRanges_from_RoSeqs);
+
+/* XRaw_class.c */
+	REGISTER_CCALLABLE(_new_STRSXP_from_RoSeqs);
+
+/* XString_class.c */
+	REGISTER_CCALLABLE(_DNAencode);
+	REGISTER_CCALLABLE(_DNAdecode);
+	REGISTER_CCALLABLE(_RNAencode);
+	REGISTER_CCALLABLE(_RNAdecode);
+	REGISTER_CCALLABLE(_get_XString_asRoSeq);
+
+/* XStringSet_class.c */
+	REGISTER_CCALLABLE(_get_XStringSet_baseClass);
+	REGISTER_CCALLABLE(_get_XStringSet_length);
+	REGISTER_CCALLABLE(_new_CachedXStringSet);
+	REGISTER_CCALLABLE(_get_CachedXStringSet_elt_asRoSeq);
+	REGISTER_CCALLABLE(_get_XStringSet_elt_asRoSeq);
+	REGISTER_CCALLABLE(_new_XStringSet_from_RoSeqs);
+	REGISTER_CCALLABLE(_set_XStringSet_names);
+	REGISTER_CCALLABLE(_alloc_XStringSet);
+	REGISTER_CCALLABLE(_write_RoSeq_to_CachedXStringSet_elt);
+	REGISTER_CCALLABLE(_write_RoSeq_to_XStringSet_elt);
+
+/* seqs_to_seqs.c */
+	REGISTER_CCALLABLE(_new_RoSeqs_from_CharAEAE);
+
+/* match_reporting.c */
+	REGISTER_CCALLABLE(_init_match_reporting);
+	REGISTER_CCALLABLE(_report_match);
+	REGISTER_CCALLABLE(_reported_matches_asSEXP);
+
+	return;
 }
 
