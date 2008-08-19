eab5c6b91bb9142845cfffafa12cd52ae4551a81
Patrick Aboyoun <unknown>
2008-08-19 20:48:03 +0000

    Moved the coverage generic definition and the IRanges and MaskCollection methods to the IRanges package.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@33279 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/match-utils.R
+++ R/match-utils.R
@@ -479,69 +479,8 @@ setMethod("mismatchSummary", "PairwiseAlignmentSummary",
 
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### The "coverage" generic and methods.
+### The "coverage" methods.
 ###
-
-setGeneric("coverage", signature="x",
-    function(x, start=NA, end=NA, ...) standardGeneric("coverage")
-)
-
-setMethod("coverage", "IRanges",
-    function(x, start=NA, end=NA, weight=1L)
-    {
-        if (!isSingleNumber(start))
-            stop("'start' must be a single integer")
-        if (!is.integer(start))
-            start <- as.integer(start)
-        if (!isSingleNumber(end))
-            stop("'end' must be a single integer")
-        if (!is.integer(end))
-            end <- as.integer(end)
-        width <- end - start + 1L
-        if (width < 0)
-            stop("'end' must be >= 'start' - 1")
-        if (!is.numeric(weight) || !(length(weight) %in% c(1, length(x))))
-            stop("'weight' must be an integer vector with length 1 or 'length(x)'")
-        if (!is.integer(weight))
-            weight <- as.integer(weight)
-        x1 <- shift(restrict(x, start=start, end=end), 1L - start)
-        .Call("IRanges_coverage", x1, width, weight, PACKAGE="IRanges")
-    }
-)
-
-setMethod("coverage", "MaskCollection",
-    function(x, start=NA, end=NA, weight=1L)
-    {
-        if (!isSingleNumberOrNA(start))
-            stop("'start' must be a single integer or NA")
-        if (!is.integer(start))
-            start <- as.integer(start)
-        if (is.na(start))
-            start <- 1L
-        if (!isSingleNumberOrNA(end))
-            stop("'end' must be a single integer or NA")
-        if (!is.integer(end))
-            end <- as.integer(end)
-        if (is.na(end))
-            end <- width(x)
-        width <- end - start + 1L
-        if (width < 0)
-            stop("'end' must be >= 'start' - 1")
-        if (!is.numeric(weight))
-            stop("'weight' must be an integer vector")
-        if (!is.integer(weight))
-            weight <- as.integer(weight)
-        if (length(weight) != length(x)) {
-            if (length(weight) < 1 || length(weight) > length(x))
-                stop("'length(weight)' must be >= 1 and <= 'length(x)'")
-            weight <- rep(weight, length.out=length(x))
-        }
-        ans <- integer(width)
-        for (i in seq_len(length(x)))
-            ans <- ans + coverage(x[[i]], start=start, end=end, weight=weight[i])
-        ans
-    }
-)
 
 setMethod("coverage", "XStringViews",
     function(x, start=NA, end=NA, weight=1L)
