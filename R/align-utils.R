74f5ea6360cde1e5681106804105c6e15c00e66d
Patrick Aboyoun <unknown>
2008-09-25 07:25:52 +0000

    Added a consensusString function.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@33948 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/align-utils.R
+++ R/align-utils.R
@@ -40,61 +40,65 @@ setMethod("consmat", "ANY",
 setGeneric("consensusMatrix", signature="x", function(x, ...)  standardGeneric("consensusMatrix"))
 
 setMethod("consensusMatrix", "character",
-    function(x, freq=FALSE)
-    {
-        consensusMatrix(BStringSet(x), freq=freq)
-    }
-)
+          function(x, freq=FALSE)
+          {
+              consensusMatrix(BStringSet(x), freq=freq)
+          })
 
 setMethod("consensusMatrix", "matrix",
-    function(x, freq=FALSE)
-    {
-        consensusMatrix(BStringSet(apply(x, 1, paste, collapse="")), freq=freq)
-    }
-)
+          function(x, freq=FALSE)
+          {
+              consensusMatrix(BStringSet(apply(x, 1, paste, collapse="")), freq=freq)
+          })
 
 ### 'x' must be a list of FASTA records as one returned by readFASTA()
 setMethod("consensusMatrix", "list",
-    function(x, freq=FALSE)
-    {
-        consensusMatrix(BStringSet(FASTArecordsToCharacter(x, use.names=FALSE)), freq=freq)
-    }
-)
+          function(x, freq=FALSE)
+          {
+              consensusMatrix(BStringSet(FASTArecordsToCharacter(x, use.names=FALSE)), freq=freq)
+          })
 
 setMethod("consensusMatrix", "XStringSet",
-    function(x, baseOnly=FALSE, freq=FALSE)
-    {
-        codes <- codes(super(x), baseOnly=baseOnly)
-        if (is.null(names(codes))) {
-            names(codes) <- intToUtf8(codes, multiple = TRUE)
-            removeUnused <- TRUE
-        } else {
-            removeUnused <- FALSE
-        }
-        freq <- .normargFreq(freq)
-        ans <- .Call("XStringSet_char_frequency_by_pos",
-                x, codes, baseOnly, PACKAGE="Biostrings")
-        if (removeUnused) {
-            ans <- ans[rowSums(ans) > 0, , drop=FALSE]
-        }
-        if (freq) {
-            ans <- t(t(ans) / colSums(ans))
-        }
-        ans
-    }
-)
+          function(x, baseOnly=FALSE, freq=FALSE)
+          {
+              codes <- codes(super(x), baseOnly=baseOnly)
+              if (is.null(names(codes))) {
+                  names(codes) <- intToUtf8(codes, multiple = TRUE)
+                  removeUnused <- TRUE
+              } else {
+                  removeUnused <- FALSE
+              }
+              freq <- .normargFreq(freq)
+              ans <- .Call("XStringSet_char_frequency_by_pos",
+                      x, codes, baseOnly, PACKAGE="Biostrings")
+              if (removeUnused) {
+                  ans <- ans[rowSums(ans) > 0, , drop=FALSE]
+              }
+              if (freq) {
+                  ans <- t(t(ans) / colSums(ans))
+              }
+              ans
+          })
 
 setMethod("consensusMatrix", "XStringViews",
-    function(x, baseOnly=FALSE, freq=FALSE)
-    {
-        y <- XStringViewsToSet(x, use.names=FALSE, verbose=FALSE)
-        consensusMatrix(y, baseOnly=baseOnly, freq=freq)
-    }
-)
+          function(x, baseOnly=FALSE, freq=FALSE)
+          {
+              y <- XStringViewsToSet(x, use.names=FALSE, verbose=FALSE)
+              consensusMatrix(y, baseOnly=baseOnly, freq=freq)
+          })
 
 setMethod("consensusMatrix", "PairwiseAlignment",
-    function(x, baseOnly=FALSE, freq=FALSE)
-    {
-        consensusMatrix(aligned(x), baseOnly=baseOnly, freq=freq)
-    }
-)
+          function(x, baseOnly=FALSE, freq=FALSE)
+          {
+              consensusMatrix(aligned(x), baseOnly=baseOnly, freq=freq)
+          })
+
+setGeneric("consensusString", signature = "x", function(x)  standardGeneric("consensusString"))
+
+setMethod("consensusString", "ANY",
+          function(x)
+          {
+              mat <- consensusMatrix(x, freq=TRUE)
+              letters <- rownames(mat)
+              paste(apply(mat, 2, function(y) if(any(y > 0.5)) letters[which.max(y)] else "?"), collapse="")
+          })
