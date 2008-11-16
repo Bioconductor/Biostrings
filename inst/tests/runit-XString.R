653e75fcfbd3c7c891680c67f80002e24b49390c
Laurent <laurent@cbs.dtu.dk>
2008-11-16 10:44:19 +0000

    renamed file runit-DNAString.R to runit-XString.R
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@35366 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/tests/runit-XString.R
@@ -0,0 +1,73 @@
+test_constructors <- function()
+{
+
+  dst <- DNAString("ACGTAT")   # just check this does not throw an error
+  checkException( dst <- DNAString("ACFTC") )
+
+  rst <- RNAString("ACGUAU")   # just check this does not throw an error
+  checkException( rst <- RNAString("ACGTC") )
+
+  ast <- AAString("ACGTAT")   # just check this does not throw an error
+  checkException( ast <- AAString("ACXTCZ") )
+
+}
+
+test_alphabet <- function()
+{
+  s <- BString("ACGTAT")
+  checkEquals(NULL, alphabet(s))
+
+  s <- DNAString("ACGTAT")
+  checkEquals(DNA_ALPHABET, alphabet(s))
+}
+
+test_length <- function()
+{
+  rs <- "ACGTAT"
+  bs <- BString("ACGTAT")
+  checkEquals(nchar(rs), length(bs))
+
+  checkEquals(nchar(rs), nchar(bs))
+}
+
+test_subsetting <- function()
+{
+    s <- BString("ACGT")
+    checkEquals("AT", as.character(s[c(1,4)]))
+
+    ## out of bounds
+    checkException(s[-1], silent=TRUE)
+    checkException(s[10], silent=TRUE)
+    
+    checkException(s[1] <- "T", silent=TRUE)
+}
+
+test_comparison <- function()
+{
+  ref_str <- "ACGTA"
+  bs1_acgta <- BString(ref_str)
+  bs2_acgta <- BString(ref_str)
+  bs_aatt <- BString("AATT")
+  
+  checkTrue(bs1_acgta == bs2_acgta)
+  checkTrue(ref_str == bs1_acgta)
+  checkTrue(bs1_acgta != bs_aatt)
+  
+  ds1_acgta <- DNAString(ref_str)
+  ds2_acgta <- DNAString(ref_str)
+  checkTrue(ds1_acgta == ds2_acgta)
+  
+  ## comparison between instances of different XString subclasses
+  ## not possible
+  checkException(bs1_acgta == ds1_acgta, silent=TRUE)
+  
+}
+
+test_subseq <- function()
+{
+  bs <- BString("ACGTA")
+  checkEquals("GTA", subseq(bs, start = 3)) # comparison between "character"
+                                            # and "BString" done elsewhere
+  checkEquals("TA", subseq(bs, start = -2))
+  
+}
