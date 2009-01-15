cb45268e0389d2a7d0cb91810dc15ae4bd1a3d21
Patrick Aboyoun <unknown>
2009-01-15 19:35:13 +0000

    Modified the meanings of max.Lmismatch and max.Rmismatch arguments to better match typical use cases.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@36683 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/trimLRPatterns.R
+++ R/trimLRPatterns.R
@@ -3,30 +3,28 @@
 ###
 
 setGeneric("trimLRPatterns", signature = "subject",
-    function(Lpattern = NULL, Rpattern = NULL, subject,
-             max.Lmismatch = 0, max.Rmismatch = 0,
+    function(Lpattern = "", Rpattern = "", subject,
+             max.Lmismatch = rep.int(c(-1, 0), c(nchar(Lpattern) - 1, 1)),
+             max.Rmismatch = rep.int(c(-1, 0), c(nchar(Rpattern) - 1, 1)),
              with.Lindels = FALSE, with.Rindels = FALSE,
              Lfixed = TRUE, Rfixed = TRUE, ranges = FALSE)
         standardGeneric("trimLRPatterns")
 )
 
 .XString.XStringSet.trimLRPatterns <- 
-function(Lpattern = NULL, Rpattern = NULL, subject,
-         max.Lmismatch = 0, max.Rmismatch = 0,
+function(Lpattern = "", Rpattern = "", subject,
+         max.Lmismatch = rep.int(c(-1, 0), c(nchar(Lpattern) - 1, 1)),
+         max.Rmismatch = rep.int(c(-1, 0), c(nchar(Rpattern) - 1, 1)),
          with.Lindels = FALSE, with.Rindels = FALSE,
          Lfixed = TRUE, Rfixed = TRUE, ranges = FALSE)
 {
-    whichMin <- function(x) {
-        if (is.matrix(x))
-            max.col(- x, ties.method = "first")
-        else
-            which.min(x)
-    }
-
-    if (is.null(Rpattern)) {
+    if (nchar(Rpattern) == 0) {
         trim.end <- nchar(subject)
     } else {
-        max.Rmismatch <- normargMaxMismatch(max.Rmismatch, name = "max.Rmismatch")
+        max.Rmismatch <- as.integer(max.Rmismatch)
+        if (any(is.na(max.Rmismatch)) || is.unsorted(max.Rmismatch) ||
+            length(max.Rmismatch) != nchar(Rpattern))
+            stop("'max.Rmismatch' must be a non-decreasing vector of length 'nchar(Rpattern)'")
         with.Rindels <- normargWithIndels(with.Rindels, name = "with.Rindels")
         Rfixed <- normargFixed(Rfixed, class(subject), name = "Rfixed")
 
@@ -34,49 +32,47 @@ function(Lpattern = NULL, Rpattern = NULL, subject,
             stop("'Rpattern' can only be used with equal length 'subject' strings")
         ncharSubject <- nchar(subject)[1]
         ncharRpattern <- nchar(Rpattern)
-        max.Rmismatch <- min(ncharRpattern, max.Rmismatch)
-        Rstarting.at <-
-          (ncharSubject - ncharRpattern + 1L + max.Rmismatch):
-          (ncharSubject - ncharRpattern + 1L)
-        Rnedit <-
-          neditStartingAt(Rpattern, subject, starting.at = Rstarting.at,
-                          with.indels = with.Rindels, fixed = Rfixed)
-        Rwhich.min <- whichMin(Rnedit)
+        Rstarting.at <- ncharSubject:(ncharSubject - ncharRpattern + 1L)
+        Rcandidate <-
+          t(t(neditStartingAt(Rpattern, subject, starting.at = Rstarting.at,
+                              with.indels = with.Rindels, fixed = Rfixed)) <=
+            max.Rmismatch)
         if (is(subject, "XString")) {
-            Rwhich.trim <- (Rnedit[Rwhich.min] <= max.Rmismatch)
+            Rwhich.candidate <- which.max(Rcandidate)
+            Rwhich.trim <- any(Rcandidate)
         } else {
-            Rwhich.minVector <-
-              seq_len(length(subject)) + (Rwhich.min - 1L) * length(subject)
-            Rwhich.trim <- (Rnedit[Rwhich.minVector] <= max.Rmismatch)
+            Rwhich.candidate <- max.col(Rcandidate, ties.method = "first")
+            Rwhich.trim <- rowSums(Rcandidate) > 0
         }
         trim.end <-
-          ifelse(Rwhich.trim, Rstarting.at[Rwhich.min] - 1L, nchar(subject))
+          ifelse(Rwhich.trim, ncharSubject - Rwhich.candidate, nchar(subject))
     }
 
-    if (is.null(Lpattern)) {
+    if (nchar(Lpattern) == 0) {
         trim.start <- min(1L, nchar(subject))
     } else {
-        max.Lmismatch <- normargMaxMismatch(max.Lmismatch, name = "max.Lmismatch")
+        max.Lmismatch <- as.integer(max.Lmismatch)
+        if (any(is.na(max.Lmismatch)) || is.unsorted(max.Lmismatch) ||
+            length(max.Lmismatch) != nchar(Lpattern))
+            stop("'max.Lmismatch' must be a non-decreasing vector of length 'nchar(Lpattern)'")
         with.Lindels <- normargWithIndels(with.Lindels, name = "with.Lindels")
         Lfixed <- normargFixed(Lfixed, class(subject), name = "Lfixed")
         
         ncharLpattern <- nchar(Lpattern)
-        max.Lmismatch <- min(ncharLpattern, max.Lmismatch)
-        Lending.at <- (ncharLpattern - max.Lmismatch):ncharLpattern
-        Lnedit <-
-          neditEndingAt(Lpattern, subject, ending.at = Lending.at,
-                        with.indels = with.Lindels, fixed = Lfixed)
-        Lwhich.min <- whichMin(Lnedit)
+        Lending.at <- seq_len(ncharLpattern)
+        Lcandidate <-
+          t(t(neditEndingAt(Lpattern, subject, ending.at = Lending.at,
+                            with.indels = with.Lindels, fixed = Lfixed)) <=
+            max.Lmismatch)
         if (is(subject, "XString")) {
-            Lwhich.trim <- (Lnedit[Lwhich.min] <= max.Lmismatch)
+            Lwhich.candidate <- which.max(Lcandidate)
+            Lwhich.trim <- any(Lcandidate)
         } else {
-            Lwhich.minVector <-
-              seq_len(length(subject)) + (Lwhich.min - 1L) * length(subject)
-            Lwhich.trim <- (Lnedit[Lwhich.minVector] <= max.Lmismatch)
+            Lwhich.candidate <- max.col(Lcandidate, ties.method = "first")
+            Lwhich.trim <- rowSums(Lcandidate) > 0
         }
         trim.start <-
-          ifelse(Lwhich.trim, Lending.at[Lwhich.min] + 1L,
-                 min(1L, nchar(subject)))
+          ifelse(Lwhich.trim, Lwhich.candidate + 1L, min(1L, nchar(subject)))
     }
     if (ranges) {
         IRanges(start = trim.start, end = trim.end)
@@ -89,8 +85,9 @@ function(Lpattern = NULL, Rpattern = NULL, subject,
 
 ### Dispatch on 'subject' (see signature of generic).
 setMethod("trimLRPatterns", "XString", 
-    function(Lpattern = NULL, Rpattern = NULL, subject,
-             max.Lmismatch = 0, max.Rmismatch = 0,
+    function(Lpattern = "", Rpattern = "", subject,
+             max.Lmismatch = rep.int(c(-1, 0), c(nchar(Lpattern) - 1, 1)),
+             max.Rmismatch = rep.int(c(-1, 0), c(nchar(Rpattern) - 1, 1)),
              with.Lindels = FALSE, with.Rindels = FALSE,
              Lfixed = TRUE, Rfixed = TRUE, ranges = FALSE)
     .XString.XStringSet.trimLRPatterns(Lpattern = Lpattern,
@@ -106,8 +103,9 @@ setMethod("trimLRPatterns", "XString",
 
 ### Dispatch on 'subject' (see signature of generic).
 setMethod("trimLRPatterns", "XStringSet", 
-    function(Lpattern = NULL, Rpattern = NULL, subject,
-             max.Lmismatch = 0, max.Rmismatch = 0,
+    function(Lpattern = "", Rpattern = "", subject,
+             max.Lmismatch = rep.int(c(-1, 0), c(nchar(Lpattern) - 1, 1)),
+             max.Rmismatch = rep.int(c(-1, 0), c(nchar(Rpattern) - 1, 1)),
              with.Lindels = FALSE, with.Rindels = FALSE,
              Lfixed = TRUE, Rfixed = TRUE, ranges = FALSE)
     .XString.XStringSet.trimLRPatterns(Lpattern = Lpattern,
