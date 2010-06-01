cd3dbc480b9c76c52af57cc4e3262532b6565c28
Patrick Aboyoun <unknown>
2010-06-01 16:59:12 +0000

    Fixed a bug when creating SolexaQuality with a p-value close to 1.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@47233 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/XStringQuality-class.R
+++ R/XStringQuality-class.R
@@ -35,7 +35,8 @@ setMethod("q2p", "SolexaQuality", function(x) function(q) 1 - 1/(1 + 10^(-q/10))
 
 setGeneric("p2q", function(x) standardGeneric("p2q"))
 setMethod("p2q", "PhredQuality", function(x) function(p) -10 * log10(p))
-setMethod("p2q", "SolexaQuality", function(x) function(p) -10 * log10(p/(1 - p)))
+setMethod("p2q", "SolexaQuality",
+          function(x) function(p) -10 * (log10(p) - log10(1 - p)))
 
 qualityConverter <- function(x, qualityClass, outputType) {
     .BStringSet2integer <- function(x, scale) {
@@ -51,7 +52,8 @@ qualityConverter <- function(x, qualityClass, outputType) {
         else
             value <-
               BStringSet(rawToChar(as.raw(pmax.int(minQuality(scale),
-                                          pmin.int(maxQuality(scale), x)) + offset(scale))))
+                                          pmin.int(maxQuality(scale), x)) +
+                                          offset(scale))))
         value
     }
     .numeric2BStringSet <- function(x, scale) {
@@ -60,7 +62,8 @@ qualityConverter <- function(x, qualityClass, outputType) {
         } else if (any(is.na(x)) || any(x < 0) || any(x > 1)) {
             stop("'x' must be numbers between 0 and 1 inclusive")
         } else {
-            x[x < 1e-12] <- 1e-12
+            x <- pmax.int(x, 1e-16)
+            x <- pmin.int(x, 1 - 1e-16)
             value <- .integer2BStringSet(as.integer(round(p2q(scale)(x))), scale)
         }
         value
@@ -69,9 +72,12 @@ qualityConverter <- function(x, qualityClass, outputType) {
     outputType <- match.arg(outputType, c("BStringSet", "integer", "numeric"))
     transform <- paste(class(x), "2", outputType, sep = "")
     switch(transform,
-           "BStringSet2BStringSet" =, "character2BStringSet" =, "integer2integer" =, "numeric2numeric" = x,
-           "BStringSet2integer" =, "character2integer" = .BStringSet2integer(x, scale),
-           "BStringSet2numeric" =, "character2numeric" = q2p(scale)(.BStringSet2integer(x, scale)),
+           "BStringSet2BStringSet" =, "character2BStringSet" =,
+           "integer2integer" =, "numeric2numeric" = x,
+           "BStringSet2integer" =, "character2integer" =
+           .BStringSet2integer(x, scale),
+           "BStringSet2numeric" =, "character2numeric" =
+           q2p(scale)(.BStringSet2integer(x, scale)),
            "integer2BStringSet" = .integer2BStringSet(x, scale),
            "numeric2BStringSet" = .numeric2BStringSet(x, scale),
            "integer2numeric" = q2p(scale)(x),
@@ -84,9 +90,13 @@ qualityConverter <- function(x, qualityClass, outputType) {
 ### Coercion.
 ###
 
-.characterToXStringQuality <- function(from, qualityClass) as(BStringSet(from), qualityClass)
-.BStringToXStringQuality <- function(from, qualityClass) as(BStringSet(from), qualityClass)
-.BStringSetToXStringQuality <- function(from, qualityClass) {
+.characterToXStringQuality <- function(from, qualityClass)
+    as(BStringSet(from), qualityClass)
+.BStringToXStringQuality <- function(from, qualityClass)
+    as(BStringSet(from), qualityClass)
+.BStringSetToXStringQuality <-
+function(from, qualityClass)
+{
     ans <- new2(qualityClass, pool=from@pool, ranges=from@ranges, check=FALSE)
     names(ans) <- names(from)
     ans
@@ -100,24 +110,38 @@ qualityConverter <- function(x, qualityClass, outputType) {
 .XStringQualityToNumeric <- function(from, qualityClass)
 	qualityConverter(BStringSet(from), qualityClass, "numeric")
 
-setAs("character", "PhredQuality", function(from) .characterToXStringQuality(from, "PhredQuality"))
-setAs("BString", "PhredQuality", function(from) .BStringToXStringQuality(from, "PhredQuality"))
-setAs("BStringSet", "PhredQuality", function(from) .BStringSetToXStringQuality(from, "PhredQuality"))
-setAs("integer", "PhredQuality", function(from) .integerToXStringQuality(from, "PhredQuality"))
-setAs("numeric", "PhredQuality", function(from) .numericToXStringQuality(from, "PhredQuality"))
-setAs("PhredQuality", "integer", function(from) .XStringQualityToInteger(from, "PhredQuality"))
+setAs("character", "PhredQuality",
+      function(from) .characterToXStringQuality(from, "PhredQuality"))
+setAs("BString", "PhredQuality",
+      function(from) .BStringToXStringQuality(from, "PhredQuality"))
+setAs("BStringSet", "PhredQuality",
+      function(from) .BStringSetToXStringQuality(from, "PhredQuality"))
+setAs("integer", "PhredQuality",
+      function(from) .integerToXStringQuality(from, "PhredQuality"))
+setAs("numeric", "PhredQuality",
+      function(from) .numericToXStringQuality(from, "PhredQuality"))
+setAs("PhredQuality", "integer",
+      function(from) .XStringQualityToInteger(from, "PhredQuality"))
 setMethod("as.integer", "PhredQuality", function(x) as(x, "integer"))
-setAs("PhredQuality", "numeric", function(from) .XStringQualityToNumeric(from, "PhredQuality"))
+setAs("PhredQuality", "numeric",
+      function(from) .XStringQualityToNumeric(from, "PhredQuality"))
 setMethod("as.numeric", "PhredQuality", function(x) as(x, "numeric"))
 
-setAs("character", "SolexaQuality", function(from) .characterToXStringQuality(from, "SolexaQuality"))
-setAs("BString", "SolexaQuality", function(from) .BStringToXStringQuality(from, "SolexaQuality"))
-setAs("BStringSet", "SolexaQuality", function(from) .BStringSetToXStringQuality(from, "SolexaQuality"))
-setAs("integer", "SolexaQuality", function(from) .integerToXStringQuality(from, "SolexaQuality"))
-setAs("numeric", "SolexaQuality", function(from) .numericToXStringQuality(from, "SolexaQuality"))
-setAs("SolexaQuality", "integer", function(from) .XStringQualityToInteger(from, "SolexaQuality"))
+setAs("character", "SolexaQuality",
+      function(from) .characterToXStringQuality(from, "SolexaQuality"))
+setAs("BString", "SolexaQuality",
+      function(from) .BStringToXStringQuality(from, "SolexaQuality"))
+setAs("BStringSet", "SolexaQuality",
+      function(from) .BStringSetToXStringQuality(from, "SolexaQuality"))
+setAs("integer", "SolexaQuality",
+      function(from) .integerToXStringQuality(from, "SolexaQuality"))
+setAs("numeric", "SolexaQuality",
+      function(from) .numericToXStringQuality(from, "SolexaQuality"))
+setAs("SolexaQuality", "integer",
+      function(from) .XStringQualityToInteger(from, "SolexaQuality"))
 setMethod("as.integer", "SolexaQuality", function(x) as(x, "integer"))
-setAs("SolexaQuality", "numeric", function(from) .XStringQualityToNumeric(from, "SolexaQuality"))
+setAs("SolexaQuality", "numeric",
+      function(from) .XStringQualityToNumeric(from, "SolexaQuality"))
 setMethod("as.numeric", "SolexaQuality", function(x) as(x, "numeric"))
 
 
