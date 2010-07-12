56ee203c47e5f8e589d760794ee7e8d4a6e13297
Marc Carlson <mcarlson@fhcrc.org>
2010-07-12 22:32:08 +0000

    Update drops old methods from MultipleAlignmentSet
    Replaces some methods with new ones, and completely drops others in
    preparation for replacing the class.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@48072 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/MultipleAlignmentSet.R
+++ R/MultipleAlignmentSet.R
@@ -5,7 +5,7 @@
 
 setClass("MultipleAlignmentSet",
     representation(set="BStringSet",
-                   masks="MaskCollection",
+                   masks="NormalIRanges",
                    rowMasks="integer"))
 
 ## MultipleAlignmentSet constructor
@@ -18,73 +18,27 @@ MultipleAlignmentSet <- function(x=character(), start=1, end=nchar(x[[1]]),use.n
   }
   new("MultipleAlignmentSet", set=BStringSet(x=x,start=start,end=end,width=NA,
                          use.names=use.names),
-                       masks=Mask(mask.width=nchar(x[[1]]),start=0,
-                         end=0),
+                       masks=as(IRanges(start=0, end=0),
+                         "NormalIRanges"),
                        rowMasks=integer(length=length(x))
       )
 }
 
-setMethod("[","MultipleAlignmentSet",
-          function(x, i, j, ..., drop){
-            if (!missing(j) || length(list(...)) > 0)
-              stop("invalid subsetting")
-            if (missing(i) || (is.logical(i) && all(i)))
-              return(x)
-            if (is.logical(i))
-              i <- which(i)
-            if (!is.numeric(i) || any(is.na(i)))
-              stop("invalid subsetting")
-            if (any(i < 1) || any(i > length(x)))
-              stop("subscript out of bounds")
-            x@set <- x@set[i]; x})
 
-setMethod("[[","MultipleAlignmentSet",
-          function(x, i, j, ..., value){
-            if (!missing(j) || length(list(...)) > 0)
-              stop("invalid subsetting")
-            if (missing(i) || (is.logical(i) && all(i)))
-              return(x)
-            if (is.logical(i))
-              i <- which(i)
-            if (!is.numeric(i) || any(is.na(i)))
-              stop("invalid subsetting")
-            if (any(i < 1) || any(i > length(x)))
-              stop("subscript out of bounds")            
-            x@set[[i]]})
-
-setMethod("names","MultipleAlignmentSet",function(x){names(x@set)})
-setReplaceMethod("names", "MultipleAlignmentSet",
+setMethod("as.character","MultipleAlignmentSet",function(x){as.character(x@set)})
+setMethod("rownames","MultipleAlignmentSet",function(x){names(x@set)})
+setReplaceMethod("rownames", "MultipleAlignmentSet",
                  function(x,value){names(x@set)<-value; x})
 
-setMethod("as.list","MultipleAlignmentSet",function(x){as.list(x@set)})
-setMethod("as.character","MultipleAlignmentSet",function(x){as.character(x@set)})
-setMethod("nchar","MultipleAlignmentSet",function(x){nchar(x@set)})
-setMethod("length","MultipleAlignmentSet",function(x){length(x@set)})
+setMethod("ncol","MultipleAlignmentSet",function(x){nchar(x@set)})
+setMethod("nrow","MultipleAlignmentSet",function(x){length(x@set)})
+
 
 
 
 ######################################################################
 ## Below here we implement versions of methods that are "mask aware"
 ######################################################################
-
-## TODO: change narrow so that it does what it says it does (instead of the
-## opposite) OR just jettison narrow (we don't really need it here, I am just
-## using it for some quick test.
-setMethod("narrow","MultipleAlignmentSet",
-          function(x,start,end,width,use.names){
-            x@masks <- Mask(mask.width=nchar(x[[1]]),start,end,width);x})
-
-
-
-## TODO: remove this after we refactor subsetColumns()
-setGeneric("xscat", signature="...",
-    function(...) standardGeneric("xscat"))
-setMethod("xscat","MultipleAlignmentSet",
-          function(...){
-            ans_set <- do.call(xscat, lapply(list(...), function(x)x@set))
-            new("MultipleAlignmentSet", set=ans_set)
-          })
-
 
 
 ## I definitely want to OL these to consider the mask slot
@@ -100,28 +54,16 @@ setMethod("alphabetFrequency","MultipleAlignmentSet",
 
 
 
+########################################################################## 
+## stuff for the show method
 
+## TODO: For the show method, We need to make sure that we display a special
+## message when the entire thing is masked...
 
-## I might need to do something like this (but modded to remove chars instead
-## of # them) I need to be able to call this on the Bstrings, BUT, I also only
-## need to be able to do it in the context of these local show methods, so
-## probably this is just a local function instead of a method...
-## setMethod("as.character", "MaskedXString",
-##     function(x)
-##     {
-##         ans <- as.character(unmasked(x))
-##         nir0 <- as(x, "NormalIRanges")
-##         for (i in seq_len(length(nir0))) {
-##             strip <- paste(rep.int("#", width(nir0)[i]), collapse="")
-##             substr(ans,  start(nir0)[i], end(nir0)[i]) <- strip
-##         }
-##         ans = gsub("#","",ans)
-##         ans
-##     }
-## )
-
-
+##msk is a normalIRanges object which *IS* a single mask from a maskCollection.
 .charStrFormat = function(str, msk){
+    msk <- Mask(mask.width=nchar(str),
+                start=start(msk),end=end(msk),width=width(msk))
     masks(str) <- msk
     ans <- as.character(unmasked(str))
     nir0 <- as(str, "NormalIRanges")
@@ -134,12 +76,6 @@ setMethod("alphabetFrequency","MultipleAlignmentSet",
     ans
 }
 
-
-
-
-
-## TODO: For the show method, We need to make sure that we display a special
-## message when the entire thing is masked...
 
 SeqSnippetSlice <- function(x, width, index, mask)
 {
@@ -176,20 +112,20 @@ SeqSnippetSlice <- function(x, width, index, mask)
 
 .XStringMAS.show_frame_line <- function(x, i, iW, widthW, index)
 {
-    width <- nchar(x)[i]
+    width <- ncol(x)[i]
     snippetWidth <- getOption("width") - 2 - iW - widthW
-    if (!is.null(names(x)))
-        snippetWidth <- snippetWidth - .namesMASW - 1   
-    seq_snippet <- SeqSnippetSlice(x[[i]], snippetWidth, index, mask=x@masks)
+    if (!is.null(rownames(x)))
+        snippetWidth <- snippetWidth - .namesMASW - 1
+    seq_snippet <- SeqSnippetSlice(x@set[[i]], snippetWidth, index, mask=x@masks)
     
-    if (!is.null(names(x)) && !is.null(seq_snippet))
+    if (!is.null(rownames(x)) && !is.null(seq_snippet))
       seq_snippet <- format(seq_snippet, width=snippetWidth)
     cat(format(paste("[", i,"]", sep=""), width=iW, justify="right"), " ",
         " ",
         seq_snippet,
         sep="")
-    if (!is.null(names(x)) && !is.null(seq_snippet)) {
-      snippet_name <- names(x)[i]
+    if (!is.null(rownames(x)) && !is.null(seq_snippet)) {
+      snippet_name <- rownames(x)[i]
       if (is.na(snippet_name))
         snippet_name <- "<NA>"
       else if (nchar(snippet_name) > .namesMASW)
@@ -206,11 +142,11 @@ SeqSnippetSlice <- function(x, width, index, mask)
   ##hard codes the number of sets of rows we plan to display
   .frameLines <- 3
   
-    lx <- length(x)
+    lx <- nrow(x)
     iW <- nchar(as.character(lx)) + 2 # 2 for the brackets
-    ncharMax <- max(nchar(x))
+    ncharMax <- max(ncol(x))
     widthW <- max(nchar(ncharMax), nchar("width"))
-    .XStringMAS.show_frame_header(iW, widthW, !is.null(names(x)))
+    .XStringMAS.show_frame_header(iW, widthW, !is.null(rownames(x)))
 
   for(index in seq_len(.frameLines)){   
     if (lx <= 2*half_nrow+1) {
@@ -234,8 +170,8 @@ SeqSnippetSlice <- function(x, width, index, mask)
 setMethod("show", "MultipleAlignmentSet",
     function(object)
     {
-        cat("  A ", class(object), " instance with ", length(object), " sequences represented.","\n", sep="")
-        if (length(object) != 0)
+        cat("  A ", class(object), " instance with ", nrow(object), " sequences represented.","\n", sep="")
+        if (nrow(object) != 0)
             .XStringMAS.show_frame(object)
     }
 )
@@ -243,7 +179,11 @@ setMethod("show", "MultipleAlignmentSet",
 
 
 
-## function to make this from a clustal file.
+##########################################################################
+## more specialized functions
+
+## function to generate this from a clustal file.
+
 clustReader = function(file){
   con = file(file)
   data = readLines(con)
@@ -269,11 +209,10 @@ clustReader = function(file){
 
 ## helper function for subsetting the columns
 subsetColumns <- function(x, start, end){
-  names = names(x)
   if(length(start) != length(end)){
     stop("You must supply an equal number of starts and ends.")
   }
-  x@masks <- Mask(mask.width=nchar(x[[1]]),start,end);
+  x@masks <- as(IRanges(start,end),"NormalIRanges");
   x
 }
 
