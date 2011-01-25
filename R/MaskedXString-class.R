331c3a2ffc7f832f40e669a003594a88651c798e
Herve Pages <hpages@fhcrc.org>
2011-01-25 21:54:49 +0000

    Biostrings 2.19.6: Keeping up with lastest changes in IRanges.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@52068 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/MaskedXString-class.R
+++ R/MaskedXString-class.R
@@ -284,7 +284,7 @@ setMethod("subseq", "MaskedXString",
     function(x, start=NA, end=NA, width=NA)
     {
         x@unmasked <- subseq(unmasked(x), start=start, end=end, width=width)
-        x@masks <- subseq(masks(x), start=start, end=end, width=width)
+        x@masks <- narrow(masks(x), start=start, end=end, width=width)
         x
     }
 )
