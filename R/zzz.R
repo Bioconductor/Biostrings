26e709e4e5f2a906d9b6214def84678fc263de1f
Martin Morgan <mtmorgan@fhcrc.org>
2011-04-05 00:32:47 +0000

    remove addVigs2WinMenu and Biobase dependency
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@54478 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -8,10 +8,6 @@
     .Call("init_RNAlkups",
           RNA_STRING_CODEC@enc_lkup, RNA_STRING_CODEC@dec_lkup,
           PACKAGE="Biostrings")
-    if (interactive() && .Platform$OS.type == "windows" &&
-             .Platform$GUI == "Rgui") {
-        addVigs2WinMenu("Biostrings")
-    }
 }
 
 .onUnload <- function(libpath)
