15ea45e3c3cdb93f7d34eb6d099dbab1dfca1c9d
Herve Pages <hpages@fhcrc.org>
2011-05-27 20:20:48 +0000

    Move getSeq() generic from the BSgenome package down to the Biostrings package.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@55877 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/getSeq.R
@@ -0,0 +1,9 @@
+### =========================================================================
+### The getSeq() generic
+### -------------------------------------------------------------------------
+
+
+setGeneric("getSeq", function(x, ...) standardGeneric("getSeq"))
+
+### Methods are defined in BSgenome and Rsamtools.
+
