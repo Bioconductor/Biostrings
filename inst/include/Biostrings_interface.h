53df54190c2c61e4f2d6d8a7f6d6e36835f8259e
Herve Pages <hpages@fhcrc.org>
2013-05-10 19:51:47 +0000

    Update instructions on how to use the Biostrings C interface.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@76407 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/include/Biostrings_interface.h
+++ inst/include/Biostrings_interface.h
@@ -11,35 +11,43 @@
  -----------------------------------------------------------------------------
 
    This file contains the prototypes of a subset of Biostrings C routines that
-   can be called by C code in other packages.
-   In order to use these routines in your package, you need to do the
-   following:
+   can be called by C code from other packages. In order to use these routines
+   in your package, you need to do the following:
 
-   a. Add the Biostrings package to the 'Depends:', 'Imports:' and 'LinkingTo:'
-      fields of your DESCRIPTION file. Note that most of the times you should
-      already have an 'Imports:' field with at least the methods package
-      listed in it (this is safe to have even if you are not sure that you
-      need it).
+   a. Add the IRanges, XVector, and Biostrings package to the 'LinkingTo:'
+      field of your DESCRIPTION file. It's very likely that you already have
+      some of them, if not all of them, listed in your 'Depends:' and/or
+      'Imports:' field. Also it's recommended that you have the methods package
+      in 'Imports:', preferably in first position.
 
    b. If it doesn't already have one, add a NAMESPACE file to your package and
       start it with the following lines:
 
         useDynLib(mypackage) # replace mypackage by the name of your package
-        import(methods)      # only if you have it in your 'Imports:' field
-        import(Biostrings)
+        import(methods)      # if you have it in your 'Imports:' field
+        import(IRanges)      # if you need to import stuff from IRanges
+        import(XVector)      # if you need to import stuff from XVector
+        import(Biostrings)   # if you need to import stuff from Biostrings
 
    c. Add a Biostrings_stubs.c file to your src/ folder with the following
       line (and nothing else) in it:
 
         #include "_Biostrings_stubs.c" // note the underscore!
 
+      If you also need to call C routines defined in XVector (e.g. cache_XRaw)
+      and/or IRanges (e.g. vector_memcpy), add an XVector_stubs.c and/or
+      IRanges_stubs.c file in a similar fashion.
+
    d. Add the following line:
 
         #include "Biostrings_interface.h"
 
-      in each .c file where you need to call a Biostrings C routine.
-      Note that this include should come after any R or system include so the
-      top of your .c file will typically look like this:
+      in your .c files where you need to call Biostrings C routines.
+      Add similar lines for XVector_interface.h and/or IRanges_interface.h
+      in case you also need to call XVector and/or IRanges C routines.
+
+      Note that those includes should come after any R or system include so
+      the top of your .c file will typically look like this:
 
         // R includes
         #include <Rdefines.h>
@@ -51,11 +59,12 @@
         #include <stdlib.h>
         ... maybe more system includes ...
 
+        #include "XVector_interface.h"
         #include "Biostrings_interface.h"
 
-   e. Any Biostrings C routine defined in Biostrings_interface.h can now be
-      called in your .c file. For example, you could write the following
-      function:
+   e. Any C routine defined in Biostrings_interface.h, XVector_interface.h, or
+      IRanges_interface.h can now be called in your .c file. For example, you
+      could write the following function:
 
         SEXP print_XString_bytes(SEXP xstring)
         {
@@ -71,26 +80,35 @@
         }
 
       to display the sequence of an XString object (in hexadecimal format).
-      Don't forget to register the print_XString_bytes() function
-      if you want to make it a .Call entry point!
+      Don't forget to register the print_XString_bytes() function if you want
+      to make it a .Call entry point!
 
    f. 2 IMPORTANT THINGS TO REMEMBER ABOUT XString OBJECTS:
-        o they are NOT null-terminated like standard strings in C: they can
-          contain the null byte so you should never use the C standard string
-          functions on them;
-        o DNAString and RNAString objects have their data ENCODED: for
-          example, if you know that the 'xstring' argument in the above code
-          will always point to a DNAString instance, then you could replace
+
+      o They are NOT null-terminated like standard strings in C: they can
+        contain the null byte so you should never use the C standard string
+        functions on them;
+
+      o DNAString and RNAString objects have their data ENCODED: for
+        example, if you can assume that the 'xstring' argument in the above
+        code will always point to a DNAString instance, then you could replace
             Rprintf("%x ", *x_char);
-          by
+        by
             Rprintf("%x(%c) ", *x_char, DNAdecode(*x_char));
-          Note that this code will work properly only if 'xstring' is a
-          DNAString instance!
+        Of course this code would work properly only if 'xstring' is actually
+        a DNAString instance!
 
    Please consult the "System and foreign language interfaces" section in the
    Writing R Extensions manual for more information:
 
      http://cran.r-project.org/doc/manuals/R-exts.html
+
+   Don't hesitate to ask on the bioc-devel mailing list for questions or
+   suggestions about this:
+
+     http://bioconductor.org/help/mailing-list/
+
+   Thanks for using the Biostrings package!
 
  *****************************************************************************/
 #include "Biostrings_defines.h"
