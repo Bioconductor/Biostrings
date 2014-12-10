868b2f4666e99ba09abf37cd83c67c27d02b33ad
Herve Pages <hpages@fhcrc.org>
2014-12-10 21:58:46 +0000

    rename files in inst/unitTests/ according to established practice
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Biostrings@97542 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/test.R
+++ R/test.R
@@ -1,11 +1,2 @@
-testBiostrings <- function()
-{
-    require("RUnit", quietly=TRUE) || stop("RUnit package not found")
-    dirs <- system.file("unitTests", package="Biostrings")
-    testFileRegexp <- "^runit.+\\.[rR]$"
-    testsuite <- defineTestSuite("Biotrings Test Suite", dirs,
-                                 testFileRegexp=testFileRegexp)
-    testresult <- runTestSuite(testsuite)
-    printTextProtocol(testresult, showDetails=FALSE)
-}
+.test <- function() BiocGenerics:::testPackage("Biostrings")
 
