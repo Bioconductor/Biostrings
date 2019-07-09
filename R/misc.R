2c58f6b7d6a7cb07c7af4f88cba3e63101bef1df
Kieran O'Neill <oneillkza@gmail.com>
2019-07-09 00:48:03 -0700

    Coerce N50()'s input to numeric to avoid integer overflow (#27)
    Coerce N50()'s input to numeric to avoid integer overflow

--- R/misc.R
+++ R/misc.R
@@ -4,8 +4,9 @@ N50 <- function(csizes)
 {
     if (!is.numeric(csizes))
         stop("'csizes' must be a vector containing the contig sizes")
-    if (!is.integer(csizes))
-        csizes <- as.integer(csizes)
+    #If only integer, then genome size maxes out at 2.1Gbp, so coerce to double:
+    if(is.integer(csizes))
+        csizes <- as.numeric(csizes)
     decreasing_csizes <- sort(csizes, decreasing=TRUE)
     tmp <- cumsum(decreasing_csizes)
     total_size <- tmp[length(tmp)]
