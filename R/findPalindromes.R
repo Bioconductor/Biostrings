a641e117cc383c73987641515556210256c5bcb7
digitalwright <eswright@pitt.edu>
2020-12-18 11:51:56 -0500

    findPalindromes - min.looplength and allow.wobble (#42)
    * Update findPalindromes.R
    
    * Update find_palindromes.c
    
    * Update R_init_Biostrings.c
    
    * Update Biostrings.h
    
    * Update findPalindromes.Rd
    
    * Update DESCRIPTION
    
    * Update findPalindromes.R
    
    * Update findPalindromes.R
    
    * Update test_findPalindromes.R
    
    * Update find_palindromes.c
    
    * Update findPalindromes.R
    
    * Update find_palindromes.c
    
    * Update R_init_Biostrings.c
    
    * Update Biostrings.h
    
    * Update findPalindromes.R
    
    * Update find_palindromes.c
    
    * Update Biostrings.h
    
    * Update R_init_Biostrings.c
    
    * Update findPalindromes.Rd
    
    * Update findPalindromes.R

--- R/findPalindromes.R
+++ R/findPalindromes.R
@@ -18,7 +18,7 @@
 ### Return an IRanges object.
 .find_palindromes <- function(subject, min.armlength,
                               max.looplength, min.looplength,
-                              max.mismatch, L2R_lkup)
+                              max.mismatch, allow.wobble, L2R_lkup)
 {
     ## check min.armlength
     if (!isSingleNumber(min.armlength))
@@ -40,14 +40,16 @@
         stop("'min.looplength' must be <= 'max.looplength'")
     if (min.looplength < 0)
         stop("'min.looplength' must be a non-negative integer")
-    if (min.looplength >= 1)
-        stop("'min.looplength' >= 1 not yet supported (will be very soon)")
+    if (!isTRUEorFALSE(allow.wobble))
+        stop("'allow.wobble' must be a logical")
+    if (allow.wobble && !(seqtype(subject) %in% c("DNA", "RNA")))
+    	    stop("subject must be DNA or RNA if 'allow.wobble' is TRUE")
     ## check max.mismatch
     max.mismatch <- normargMaxMismatch(max.mismatch)
     C_ans <- .Call2("find_palindromes",
                     subject,
                     min.armlength, max.looplength, max.mismatch,
-                    L2R_lkup,
+                    min.looplength, allow.wobble, L2R_lkup,
                     PACKAGE="Biostrings")
     unsafe.newXStringViews(subject, start(C_ans), width(C_ans))
 }
@@ -55,49 +57,52 @@
 setGeneric("findPalindromes", signature="subject",
     function(subject, min.armlength=4,
                       max.looplength=1, min.looplength=0,
-                      max.mismatch=0)
+                      max.mismatch=0, allow.wobble=FALSE)
         standardGeneric("findPalindromes")
 )
 
 setMethod("findPalindromes", "XString",
     function(subject, min.armlength=4,
                       max.looplength=1, min.looplength=0,
-                      max.mismatch=0)
+                      max.mismatch=0, allow.wobble=FALSE)
     {
         .find_palindromes(subject, min.armlength,
                           max.looplength, min.looplength,
-                          max.mismatch, NULL)
+                          max.mismatch, allow.wobble,
+                          NULL)
     }
 )
 
 setMethod("findPalindromes", "DNAString",
     function(subject, min.armlength=4,
                       max.looplength=1, min.looplength=0,
-                      max.mismatch=0)
+                      max.mismatch=0, allow.wobble=FALSE)
     {
         L2R_lkup <- .get_DNAorRNA_palindrome_L2R_lkup()
         .find_palindromes(subject, min.armlength,
                           max.looplength, min.looplength,
-                          max.mismatch, L2R_lkup)
+                          max.mismatch, allow.wobble,
+                          L2R_lkup)
     }
 )
 
 setMethod("findPalindromes", "RNAString",
     function(subject, min.armlength=4,
                       max.looplength=1, min.looplength=0,
-                      max.mismatch=0)
+                      max.mismatch=0, allow.wobble=FALSE)
     {
         L2R_lkup <- .get_DNAorRNA_palindrome_L2R_lkup()
         .find_palindromes(subject, min.armlength,
                           max.looplength, min.looplength,
-                          max.mismatch, L2R_lkup)
+                          max.mismatch, allow.wobble,
+                          L2R_lkup)
     }
 )
 
 setMethod("findPalindromes", "XStringViews",
     function(subject, min.armlength=4,
                       max.looplength=1, min.looplength=0,
-                      max.mismatch=0)
+                      max.mismatch=0, allow.wobble=FALSE)
     {
         tmp <- vector(mode="list", length=length(subject))
         offsets <- start(subject) - 1L
@@ -106,7 +111,8 @@ setMethod("findPalindromes", "XStringViews",
                                     min.armlength=min.armlength,
                                     max.looplength=max.looplength,
                                     min.looplength=min.looplength,
-                                    max.mismatch=max.mismatch)
+                                    max.mismatch=max.mismatch,
+                                    allow.wobble=allow.wobble)
             tmp[[i]] <- shift(ranges(pals), shift=offsets[i])
         }
         ans_ranges <- do.call("c", tmp)
@@ -118,13 +124,14 @@ setMethod("findPalindromes", "XStringViews",
 setMethod("findPalindromes", "MaskedXString",
     function(subject, min.armlength=4,
                       max.looplength=1, min.looplength=0,
-                      max.mismatch=0)
+                      max.mismatch=0, allow.wobble=FALSE)
     {
         findPalindromes(toXStringViewsOrXString(subject),
                         min.armlength=min.armlength,
                         max.looplength=max.looplength,
                         min.looplength=min.looplength,
-                        max.mismatch=max.mismatch)
+                        max.mismatch=max.mismatch,
+                        allow.wobble=allow.wobble)
     }
 )
 
@@ -133,98 +140,129 @@ setMethod("findPalindromes", "MaskedXString",
 ### The palindromeArmLength() generic and methods
 ###
 
-.palindrome_arm_length <- function(x, max.mismatch, L2R_lkup)
+.palindrome_arm_length <- function(x, max.mismatch, allow.wobble, L2R_lkup)
 {
+	if (!isTRUEorFALSE(allow.wobble))
+        stop("'allow.wobble' must be a logical")
+    if (allow.wobble && !(seqtype(x) %in% c("DNA", "RNA")))
+        stop("x must be DNA or RNA if 'allow.wobble' is TRUE")
+	
     max.mismatch <- normargMaxMismatch(max.mismatch)
-    armlength <- .Call2("palindrome_arm_length",
-                        x, max.mismatch, L2R_lkup,
-                        PACKAGE="Biostrings")
-    if (armlength == 0L)
-        stop("'x' is not a palindrome (no arms found)")
-    armlength
+    .Call2("palindrome_arm_length",
+           x, max.mismatch, allow.wobble, L2R_lkup,
+           PACKAGE="Biostrings")
 }
 
 setGeneric("palindromeArmLength", signature="x",
-    function(x, max.mismatch=0) standardGeneric("palindromeArmLength")
+    function(x, max.mismatch=0, allow.wobble=FALSE) standardGeneric("palindromeArmLength")
 )
 
 setMethod("palindromeArmLength", "XString",
-    function(x, max.mismatch=0) .palindrome_arm_length(x, max.mismatch, NULL)
+    function(x, max.mismatch=0, allow.wobble=FALSE)
+    {
+    	    x <- as(x, "XStringSet")
+        .palindrome_arm_length(x, max.mismatch, allow.wobble, NULL)
+    }
 )
 
 setMethod("palindromeArmLength", "DNAString",
-    function(x, max.mismatch=0)
+    function(x, max.mismatch=0, allow.wobble=FALSE)
     {
         L2R_lkup <- .get_DNAorRNA_palindrome_L2R_lkup()
-        .palindrome_arm_length(x, max.mismatch, L2R_lkup)
+        x <- as(x, "DNAStringSet")
+        .palindrome_arm_length(x, max.mismatch, allow.wobble, L2R_lkup)
     }
 )
 
 setMethod("palindromeArmLength", "RNAString",
-    function(x, max.mismatch=0)
+    function(x, max.mismatch=0, allow.wobble=FALSE)
     {
         L2R_lkup <- .get_DNAorRNA_palindrome_L2R_lkup()
-        .palindrome_arm_length(x, max.mismatch, L2R_lkup)
+        x <- as(x, "RNAStringSet")
+        .palindrome_arm_length(x, max.mismatch, allow.wobble, L2R_lkup)
     }
 )
 
 setMethod("palindromeArmLength", "XStringViews",
-    function(x, max.mismatch=0)
+    function(x, max.mismatch=0, allow.wobble=FALSE)
     {
         if (length(x) == 0)
             return(integer(0))
-        sapply(seq_len(length(x)),
-               function(i)
-                   palindromeArmLength(x[[i]], max.mismatch=max.mismatch))
+        
+        if (seqtype(x) %in% c("DNA", "RNA")) {
+        	    	L2R_lkup <- .get_DNAorRNA_palindrome_L2R_lkup()
+        	} else {
+        	    L2R_lkup <- NULL
+        	}
+        
+        max.mismatch <- normargMaxMismatch(max.mismatch)
+        x <- fromXStringViewsToStringSet(x, out.of.limits="error")
+        .palindrome_arm_length(x, max.mismatch, allow.wobble, L2R_lkup)
     }
 )
 
+setMethod("palindromeArmLength", "XStringSet",
+    function(x, max.mismatch=0, allow.wobble=FALSE)
+    {
+        if (length(x) == 0)
+            return(integer(0))
+        
+        if (seqtype(x) %in% c("DNA", "RNA")) {
+        	    	L2R_lkup <- .get_DNAorRNA_palindrome_L2R_lkup()
+        	} else {
+        	    L2R_lkup <- NULL
+        	}
+        
+        max.mismatch <- normargMaxMismatch(max.mismatch)
+        .palindrome_arm_length(x, max.mismatch, allow.wobble, L2R_lkup)
+    }
+)
 
 ### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ### The palindromeLeftArm() and palindromeRightArm() generics and methods
 ###
 
 setGeneric("palindromeLeftArm", signature="x",
-    function(x, max.mismatch=0)
+    function(x, max.mismatch=0, allow.wobble=FALSE)
         standardGeneric("palindromeLeftArm")
 )
 
 setGeneric("palindromeRightArm", signature="x",
-    function(x, max.mismatch=0)
+    function(x, max.mismatch=0, allow.wobble=FALSE)
         standardGeneric("palindromeRightArm")
 )
 
 setMethod("palindromeLeftArm", "XString",
-    function(x, max.mismatch=0)
+    function(x, max.mismatch=0, allow.wobble=FALSE)
         subseq(x,
             start=1L,
-            end=palindromeArmLength(x, max.mismatch=max.mismatch)
+            end=palindromeArmLength(x, max.mismatch=max.mismatch, allow.wobble=allow.wobble)
         )
 )
 
 setMethod("palindromeRightArm", "XString",
-    function(x, max.mismatch=0)
+    function(x, max.mismatch=0, allow.wobble=FALSE)
     {
         start <- nchar(x) -
-                 palindromeArmLength(x, max.mismatch=max.mismatch) +
+                 palindromeArmLength(x, max.mismatch=max.mismatch, allow.wobble=allow.wobble) +
                  1L
         subseq(x, start=start, end=nchar(x))
     }
 )
 
 setMethod("palindromeLeftArm", "XStringViews",
-    function(x, max.mismatch=0)
+    function(x, max.mismatch=0, allow.wobble=FALSE)
     {
         ans_start <- start(x)
-        ans_width <- palindromeArmLength(x, max.mismatch=max.mismatch)
+        ans_width <- palindromeArmLength(x, max.mismatch=max.mismatch, allow.wobble=allow.wobble)
         unsafe.newXStringViews(subject(x), ans_start, ans_width)
     }
 )
 
 setMethod("palindromeRightArm", "XStringViews",
-    function(x, max.mismatch=0)
+    function(x, max.mismatch=0, allow.wobble=FALSE)
     {
-        ans_width <- palindromeArmLength(x, max.mismatch=max.mismatch)
+        ans_width <- palindromeArmLength(x, max.mismatch=max.mismatch, allow.wobble=allow.wobble)
         ans_start <- end(x) - ans_width + 1L
         unsafe.newXStringViews(subject(x), ans_start, ans_width)
     }
--- src/find_palindromes.c
+++ src/find_palindromes.c
@@ -5,7 +5,8 @@
 #include <stdio.h>
 
 
-static int is_match(char c1, char c2, const int *lkup, int lkup_len)
+static int is_match(char c1, char c2, int allow_wobble1,
+	const int *lkup, int lkup_len)
 {
 	int key, val;
 
@@ -15,32 +16,59 @@ static int is_match(char c1, char c2, const int *lkup, int lkup_len)
 			return 0;
 		c1 = (char) val;
 	}
-	return c1 == c2;
+	if (allow_wobble1) {
+		return c1 == c2 ||
+			(c1 == 2 && c2 == 8) || // G and T/U
+			(c1 == 1 && c2 == 4); // T/U and G
+	} else {
+		return c1 == c2;
+	}
 }
 
 static void get_find_palindromes_at(const char *x, int x_len,
 	int i1, int i2, int max_loop_len1, int min_arm_len, int max_nmis,
-	const int *lkup, int lkup_len)
+	int allow_wobble1, const int *lkup, int lkup_len)
 {
-	int arm_len, valid_indices;
+	int stack = max_nmis + 1; // size of stack (assumes max_nmis is small)
+	int arms[stack]; // lengths of matched arms
+	int arm_len, valid_indices, nmis;
 	char c1, c2;
+	static int ismatch;
 
 	arm_len = 0;
+	nmis = 0;
+	int count = 0; // position in stack
 	while (((valid_indices = i1 >= 0 && i2 < x_len) &&
                 i2 - i1 <= max_loop_len1) || arm_len != 0)
 	{
 		if (valid_indices) {
 			c1 = x[i1];
 			c2 = x[i2];
-			if (is_match(c1, c2, lkup, lkup_len) ||
-			    max_nmis-- > 0) {
+			ismatch = is_match(c1, c2, allow_wobble1, lkup, lkup_len);
+			if (!ismatch) {
+				arms[count] = arm_len;
+				count++;
+			}
+			if (ismatch || nmis++ < max_nmis) {
 				arm_len++;
 				goto next;
 			}
 		}
 		if (arm_len >= min_arm_len)
 			_report_match(i1 + 2, i2 - i1 - 1);
-		arm_len = 0;
+		if ((i1 > 0 && i2 + 1 < x_len) || nmis > max_nmis) {
+				nmis--;
+				arm_len -= arms[0];
+				// pop off bottom of stack
+				for (int i = 1; i < count; i++)
+					arms[i] -= arms[0];
+				for (int i = 1; i < count; i++)
+					arms[i - 1] = arms[i];
+				count--;
+		} else {
+			arm_len = 0;
+			nmis = 0;
+		}
 	next:
 		i1--;
 		i2++;
@@ -49,7 +77,7 @@ static void get_find_palindromes_at(const char *x, int x_len,
 }
 
 static int get_palindrome_arm_length(const char *x, int x_len, int max_nmis,
-	const int *lkup, int lkup_len)
+	int allow_wobble1, const int *lkup, int lkup_len)
 {
 	int i1, i2;
 	char c1, c2;
@@ -57,7 +85,7 @@ static int get_palindrome_arm_length(const char *x, int x_len, int max_nmis,
 	for (i1 = 0, i2 = x_len - 1; i1 < i2; i1++, i2--) {
 		c1 = x[i1];
 		c2 = x[i2];
-		if (!(is_match(c1, c2, lkup, lkup_len) ||
+		if (!(is_match(c1, c2, allow_wobble1, lkup, lkup_len) ||
 		      max_nmis-- > 0))
 			break;
 	}
@@ -66,10 +94,12 @@ static int get_palindrome_arm_length(const char *x, int x_len, int max_nmis,
 
 /* --- .Call ENTRY POINT --- */
 SEXP find_palindromes(SEXP x, SEXP min_armlength, SEXP max_looplength,
-		      SEXP max_mismatch, SEXP L2R_lkup)
+		      SEXP max_mismatch, SEXP min_looplength, SEXP allow_wobble,
+		      SEXP L2R_lkup)
 {
 	Chars_holder x_holder;
-	int x_len, min_arm_len, max_loop_len1, max_nmis, lkup_len, n;
+	int x_len, min_arm_len, max_loop_len1, max_nmis, lkup_len, n,
+		min_loop_len1, min_loop_len2, allow_wobble1;
 	const int *lkup;
 
 	x_holder = hold_XRaw(x);
@@ -77,6 +107,10 @@ SEXP find_palindromes(SEXP x, SEXP min_armlength, SEXP max_looplength,
 	min_arm_len = INTEGER(min_armlength)[0];
 	max_loop_len1 = INTEGER(max_looplength)[0] + 1;
 	max_nmis = INTEGER(max_mismatch)[0];
+	min_loop_len1 = INTEGER(min_looplength)[0];
+	min_loop_len2 = (min_loop_len1 + 1)/2; // 0 1 1 2 2 ...
+	min_loop_len1 /= 2; // 0 0 1 1 2 2 ...
+	allow_wobble1 = INTEGER(allow_wobble)[0];
 	if (L2R_lkup == R_NilValue) {
 		lkup = NULL;
 		lkup_len = 0;
@@ -87,27 +121,27 @@ SEXP find_palindromes(SEXP x, SEXP min_armlength, SEXP max_looplength,
 	_init_match_reporting("MATCHES_AS_RANGES", 1);
 	for (n = 0; n < x_len; n++) {
 		/* Find palindromes centered on n. */
-		get_find_palindromes_at(x_holder.ptr, x_len, n - 1, n + 1,
-					max_loop_len1, min_arm_len, max_nmis,
-					lkup, lkup_len);
+		get_find_palindromes_at(x_holder.ptr, x_len, n - 1 - min_loop_len1,
+					n + 1 + min_loop_len1, max_loop_len1, min_arm_len,
+					max_nmis, allow_wobble1, lkup, lkup_len);
 		/* Find palindromes centered on n + 0.5. */
-		get_find_palindromes_at(x_holder.ptr, x_len, n, n + 1,
-					max_loop_len1, min_arm_len, max_nmis,
-					lkup, lkup_len);
+		get_find_palindromes_at(x_holder.ptr, x_len, n - min_loop_len2,
+					n + 1 + min_loop_len2, max_loop_len1, min_arm_len,
+					max_nmis, allow_wobble1, lkup, lkup_len);
 	}
 	return _reported_matches_asSEXP();
 }
 
 /* --- .Call ENTRY POINT --- */
-SEXP palindrome_arm_length(SEXP x, SEXP max_mismatch, SEXP L2R_lkup)
+SEXP palindrome_arm_length(SEXP x, SEXP max_mismatch, SEXP allow_wobble, SEXP L2R_lkup)
 {
-	Chars_holder x_holder;
-	int x_len, max_nmis, lkup_len, arm_len;
+	XStringSet_holder x_holder = _hold_XStringSet(x);
+	int x_len = _get_XStringSet_length(x);
+	int max_nmis, lkup_len, allow_wobble1;
 	const int *lkup;
 
-	x_holder = hold_XRaw(x);
-	x_len = x_holder.length;
 	max_nmis = INTEGER(max_mismatch)[0];
+	allow_wobble1 = INTEGER(allow_wobble)[0];
 	if (L2R_lkup == R_NilValue) {
 		lkup = NULL;
 		lkup_len = 0;
@@ -115,8 +149,19 @@ SEXP palindrome_arm_length(SEXP x, SEXP max_mismatch, SEXP L2R_lkup)
 		lkup = INTEGER(L2R_lkup);
 		lkup_len = LENGTH(L2R_lkup);
 	}
-	arm_len = get_palindrome_arm_length(x_holder.ptr, x_len, max_nmis,
-					    lkup, lkup_len);
-	return ScalarInteger(arm_len);
+	
+	SEXP ans;
+	PROTECT(ans = NEW_INTEGER(x_len));
+	int *rans = INTEGER(ans);
+	
+	for (int i = 0; i < x_len; i++) {
+		Chars_holder x_elt_holder = _get_elt_from_XStringSet_holder(&x_holder, i);
+		rans[i] = get_palindrome_arm_length(x_elt_holder.ptr, x_elt_holder.length,
+											max_nmis, allow_wobble1, lkup, lkup_len);
+	}
+	
+	UNPROTECT(1);
+	
+	return ans;
 }
 
