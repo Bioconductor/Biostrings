ba91b03b39e90f0a97e8b1a39d236ac24e4b6bad
LiNk-NY <mramos09@gmail.com>
2021-04-26 16:49:21 -0400

    add seinfo and documentation

---
+++ R/seqinfo.R
@@ -0,0 +1,22 @@
+
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### seqinfo setter and getter
+###
+
+setMethod("seqinfo", "DNAStringSet", function(x) {
+    si <- metadata(x)$seqinfo
+    if (is.null(si)) {
+        sn <- names(x)
+        if (is.null(sn))
+            sn <- as.character(seq_len(length(x)))
+        si <- Seqinfo(sn, width(x))
+    }
+    si
+})
+
+setReplaceMethod("seqinfo", "DNAStringSet", function(x, value) {
+    metadata(x)$seqinfo <- value
+    x
+})
+
+
