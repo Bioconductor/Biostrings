2a5dfe722daa3683815b7d95cad3455e37943770
LiNk-NY <mramos09@gmail.com>
2021-04-29 15:21:37 -0400

    update seqinfo<- and examples

--- R/seqinfo.R
+++ R/seqinfo.R
@@ -14,9 +14,27 @@ setMethod("seqinfo", "DNAStringSet", function(x) {
     si
 })
 
-setReplaceMethod("seqinfo", "DNAStringSet", function(x, value) {
-    metadata(x)$seqinfo <- value
-    x
-})
+setReplaceMethod("seqinfo", "DNAStringSet",
+    function(x, new2old = NULL, pruning.mode = c("error", "coarse", "fine", "tidy"), value) {
+        if (!is.null(new2old))
+            stop("'new2old' not supported in 'seqinfo<-,DNAStringSet-method'",
+                call. = FALSE)
+
+        xnames <- names(x)
+        same_names <-
+            if (is.null(xnames))
+                identical(as.character(seq_along(x)), names(value))
+            else
+                identical(xnames, names(value))
+        if (!same_names)
+            stop("'names(x)' and 'names(value)' are not identical")
+
+        if (!identical(width(x), unname(seqlengths(value))))
+            stop("'width(x)' is not the same as 'seqlengths(value)'")
+
+        metadata(x)$seqinfo <- value
+        x
+    }
+)
 
 
