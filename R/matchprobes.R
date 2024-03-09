c8eceb3548581b8119c85551d0e00543d4c45289
Hervé Pagès <hpages.on.github@gmail.com>
2024-03-09 09:59:29 -0800

    Deprecate matchprobes() and longestConsecutive()
    Also start to clean up the TODO file a little bit.

--- R/matchprobes.R
+++ R/matchprobes.R
@@ -1,6 +1,12 @@
-matchprobes <- function(query, records, probepos=FALSE) 
+matchprobes <- function(query, records, probepos=FALSE) {
+  msg <- "matchprobes() is deprecated. Please use matchPdict() instead."
+  .Deprecated(msg=msg)
   .Call2("MP_matchprobes", toupper(query), toupper(records), probepos, PACKAGE="Biostrings")
+}
 
-longestConsecutive <- function(seq, letter) 
+longestConsecutive <- function(seq, letter) {
+  msg <- "longestConsecutive() is deprecated and won't be replaced"
+  .Deprecated(msg=msg)
   .Call2("MP_longestConsecutive", seq, letter, PACKAGE="Biostrings")
+}
 
