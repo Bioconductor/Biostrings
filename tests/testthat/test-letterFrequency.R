0a227c6e0f93d69e1fc3a97b48c44e51c7ff80a7
ahl27 <ahl27@pitt.edu>
2024-07-19 13:20:35 -0400

    add more tests for optional parameters of alphabetFrequency and letterFrequency

--- tests/testthat/test-letterFrequency.R
+++ tests/testthat/test-letterFrequency.R
@@ -26,7 +26,7 @@ check_alphfreq <- function(XStringFUN, X_ALPH, isMatrix=FALSE, checkBaseOnly=NUL
 	if(isMatrix){
 		exp_res <- matrix(0L, nrow=5, ncol=length(tab))
 		colnames(exp_res) <- names(tab)
-		strs <- character(1L)
+		strs <- character(5L)
 		for(i in seq_len(5)){
 			tab[] <- 0L
 			s <- sample(X_ALPH, 100L, replace=TRUE)
@@ -35,16 +35,26 @@ check_alphfreq <- function(XStringFUN, X_ALPH, isMatrix=FALSE, checkBaseOnly=NUL
 			exp_res[i,] <- tab
 			strs[i] <- paste(s, collapse='')
 		}
+		norm_res <- exp_res / rowSums(exp_res)
 		ss <- XStringFUN(strs)
 	} else {
-		s <- sample(X_ALPH, 100L, replace=TRUE)
-		u_v <- table(s)
+		strs <- sample(X_ALPH, 100L, replace=TRUE)
+		u_v <- table(strs)
 		tab[names(u_v)] <- u_v
 		exp_res <- tab
-		ss <- XStringFUN(paste(s, collapse=''))
+		ss <- XStringFUN(paste(strs, collapse=''))
+		norm_res <- exp_res / sum(exp_res)
 	}
 
-	expect_equal(alphabetFrequency(ss), exp_res)
+	# Check alphabetFrequency
+	v <- alphabetFrequency(ss)
+	expect_equal(v, exp_res)
+	expect_equal(sum(v), sum(nchar(strs)))
+	expect_equal(alphabetFrequency(ss, as.prob=TRUE), norm_res)
+
+	# Check letterFrequency
+	expect_equal(letterFrequency(ss, X_ALPH), exp_res)
+	expect_equal(letterFrequency(ss, X_ALPH, as.prob=TRUE), norm_res)
 
 	if(!is.null(checkBaseOnly)){
 		# provide the alphabet
@@ -52,13 +62,27 @@ check_alphfreq <- function(XStringFUN, X_ALPH, isMatrix=FALSE, checkBaseOnly=NUL
 		if(isMatrix){
 			p_in <- match(colnames(exp_res), b, nomatch=0)
 			exp_res <- cbind(exp_res[,p_in!=0], other=rowSums(exp_res[,p_in==0]))
+			norm_res <- exp_res / rowSums(exp_res)
 		} else {
 			p_in <- match(names(exp_res), b, nomatch=0)
 			exp_res <- c(exp_res[p_in!=0], sum(exp_res[p_in==0]))
 			names(exp_res) <- c(b, "other")
+			norm_res <- exp_res / sum(exp_res)
 		}
 
-		expect_equal(alphabetFrequency(ss, baseOnly=TRUE), exp_res)
+		v <- alphabetFrequency(ss, baseOnly=TRUE)
+		expect_equal(v, exp_res)
+		expect_equal(sum(v), sum(nchar(strs)))
+		expect_error(alphabetFrequency(ss, baseOnly=NA), "'baseOnly' must be TRUE or FALSE")
+		expect_error(alphabetFrequency(ss, baseOnly=1), "'baseOnly' must be TRUE or FALSE")
+		expect_equal(alphabetFrequency(ss, baseOnly=TRUE, as.prob=TRUE), norm_res)
+		if(isMatrix){
+			expect_equal(letterFrequency(ss, b), exp_res[,seq_along(b)])
+			expect_equal(letterFrequency(ss, b, as.prob=TRUE), norm_res[,seq_along(b)])
+		} else {
+			expect_equal(letterFrequency(ss, b), exp_res[seq_along(b)])
+			expect_equal(letterFrequency(ss, b, as.prob=TRUE), norm_res[seq_along(b)])
+		}
 	}
 }
 
@@ -74,7 +98,7 @@ test_that("uniqueLetters works properly", {
 	check_uniqueLetters(BStringSet, B_ALPHABET)
 })
 
-test_that("alphabetFrequency functions for all input types", {
+test_that("letterFrequency, alphabetFrequency functions for all input types", {
 	check_alphfreq(DNAString, DNA_ALPHABET, checkBaseOnly=DNA_BASES)
 	check_alphfreq(RNAString, RNA_ALPHABET, checkBaseOnly=RNA_BASES)
 	check_alphfreq(AAString, AA_ALPHABET, checkBaseOnly=AA_STANDARD)
@@ -82,6 +106,22 @@ test_that("alphabetFrequency functions for all input types", {
 	check_alphfreq(DNAStringSet, DNA_ALPHABET, isMatrix=TRUE, checkBaseOnly=DNA_BASES)
 	check_alphfreq(RNAStringSet, RNA_ALPHABET, isMatrix=TRUE, checkBaseOnly=RNA_BASES)
 	check_alphfreq(AAStringSet, AA_ALPHABET, isMatrix=TRUE, checkBaseOnly=AA_STANDARD)
+
+	## checking for ambiguity tabulation
+	d <- DNAString("ATGCATGC.+Y")
+	exp_output <- c(2L, 4L, 2L)
+	names(exp_output) <- c("A", "G|C", "T")
+	expect_equal(letterFrequency(d, letters=c("A", "GC", "T")), exp_output)
+	names(exp_output)[2] <- "G*C"
+	expect_equal(letterFrequency(d, letters=c("A", "GC", "T"), OR="*"), exp_output)
+
+	r <- RNAString("AUGCAUGC.+Y")
+	exp_output <- 8L
+	names(exp_output) <- c("A|U|G|C")
+	expect_equal(letterFrequency(r, letters="AUGC"), exp_output)
+	exp_output <- rep(2L, 4)
+	names(exp_output) <- c("A","U","G","C") # intentionally different from RNA_BASES
+	expect_equal(letterFrequency(r, letters="AUGC", OR=0), exp_output)
 
 	## BStrings have their own check
 	set.seed(999L)
@@ -92,4 +132,18 @@ test_that("alphabetFrequency functions for all input types", {
 	bstr <- paste(bstr, collapse='')
 	expect_equal(alphabetFrequency(BString(bstr)), r)
 	expect_equal(alphabetFrequency(BStringSet(bstr)), matrix(r, nrow=1L, ncol=256L))
+
+	expect_error(alphabetFrequency(BString(), baseOnly=TRUE), "unused argument (baseOnly = TRUE)", fixed=TRUE)
+})
+
+test_that("hasOnlyBaseLetters works as expected", {
+	expect_true(hasOnlyBaseLetters(DNAStringSet(DNA_BASES)))
+	expect_true(hasOnlyBaseLetters(RNAStringSet(RNA_BASES)))
+	expect_true(hasOnlyBaseLetters(AAStringSet(AA_STANDARD)))
+
+	expect_false(hasOnlyBaseLetters(DNAStringSet(DNA_ALPHABET)))
+	expect_false(hasOnlyBaseLetters(RNAStringSet(RNA_ALPHABET)))
+	expect_false(hasOnlyBaseLetters(AAStringSet(AA_ALPHABET)))
+
+	expect_error(hasOnlyBaseLetters(BStringSet()), 'unable to find an inherited method')
 })
