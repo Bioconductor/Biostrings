b32a694d8b74733ae7087bf346e68eb2aa983d40
ahl27 <ahl27@pitt.edu>
2024-08-15 09:29:41 -0400

    adds more letterFrequency.R tests

--- tests/testthat/test-letterFrequency.R
+++ tests/testthat/test-letterFrequency.R
@@ -3,9 +3,17 @@
 ##	- hasOnlyBaseLetters
 ##	- uniqueLetters
 ##	- letterFrequency
-##	- letterFrequencyInSlidingView
+##	- *letterFrequencyInSlidingView
 ##	- consensusMatrix
 ##	- consensusString (matrix, DNAStringSet, RNAStringSet)
+##  - mkAllStrings
+##  - oligonucleotideFrequency
+##	- dinucleotideFrequency
+##	- trinucleotideFrequency
+## 	- *oligonucleotideTransitions
+##  - *twoWayAlphabetFrequency
+##
+## starred functions are still lacking tests
 
 B_ALPHABET <- strsplit(rawToChar(as.raw(32:126)), '')[[1]]
 
@@ -134,9 +142,29 @@ test_that("letterFrequency, alphabetFrequency functions for all input types", {
 	expect_equal(alphabetFrequency(BStringSet(bstr)), matrix(r, nrow=1L, ncol=256L))
 
 	expect_error(alphabetFrequency(BString(), baseOnly=TRUE), "unused argument (baseOnly = TRUE)", fixed=TRUE)
+
+	## checking for Views objects and maskedString objects
+	ds <- sample(DNA_ALPHABET, 100L, replace=TRUE)
+	exp_out <- matrix(0L, nrow=2, ncol=length(DNA_ALPHABET))
+	colnames(exp_out) <- DNA_ALPHABET
+	exp_out[1,names(table(ds[seq(1,50)]))] <- table(ds[seq(1,50)])
+	exp_out[2,names(table(ds[seq(51,100)]))] <- table(ds[seq(51,100)])
+	v <- Views(DNAString(paste(ds, collapse='')), start=c(1,51), end=c(50,100))
+	expect_equal(alphabetFrequency(v), exp_out)
+	expect_equal(letterFrequency(v, "A"), exp_out[,"A",drop=FALSE])
+
+	mask_d <- DNAString(paste(ds, collapse=''))
+	masks(mask_d) <- Mask(length(ds), start=c(5,50), end=c(10,60))
+	exp_out <- rep(0L, length(DNA_ALPHABET))
+	names(exp_out) <- DNA_ALPHABET
+	subdstr <- ds[-c(5:10,50:60)]
+	exp_out[names(table(subdstr))] <- table(subdstr)
+	expect_equal(alphabetFrequency(mask_d), exp_out)
+	expect_equal(letterFrequency(mask_d, "A"), exp_out["A"])
 })
 
 test_that("hasOnlyBaseLetters works as expected", {
+	## XStringSet
 	expect_true(hasOnlyBaseLetters(DNAStringSet(DNA_BASES)))
 	expect_true(hasOnlyBaseLetters(RNAStringSet(RNA_BASES)))
 	expect_true(hasOnlyBaseLetters(AAStringSet(AA_STANDARD)))
@@ -144,6 +172,94 @@ test_that("hasOnlyBaseLetters works as expected", {
 	expect_false(hasOnlyBaseLetters(DNAStringSet(DNA_ALPHABET)))
 	expect_false(hasOnlyBaseLetters(RNAStringSet(RNA_ALPHABET)))
 	expect_false(hasOnlyBaseLetters(AAStringSet(AA_ALPHABET)))
-
 	expect_error(hasOnlyBaseLetters(BStringSet()), 'unable to find an inherited method')
+
+	## Views
+	d1 <- DNAString(paste(DNA_BASES, collapse=''))
+	d2 <- DNAString(paste(DNA_ALPHABET, collapse=''))
+	expect_true(hasOnlyBaseLetters(Views(d1, start=1, end=length(DNA_BASES))))
+	expect_false(hasOnlyBaseLetters(Views(d2, start=1, end=length(DNA_ALPHABET))))
+
+	## MaskedXString
+	md1 <- d1
+	md2 <- d2
+	masks(md1) <- Mask(length(d1), start=2, end=3)
+	masks(md2) <- Mask(length(d2), start=2, end=5)
+	expect_true(hasOnlyBaseLetters(md1))
+	expect_false(hasOnlyBaseLetters(md2))
+
+})
+
+test_that("miscellaneous letterFrequency methods work", {
+	## mkAllStrings
+	alph <- c("A", "B", "C")
+	exp_output <- expand.grid(alph, alph, alph)
+	exp_output <- apply(exp_output, 1L, paste, collapse='')
+	expect_equal(sort(mkAllStrings(alph, 3L)), sort(exp_output))
+
+	## uniqueLetters
+	d <- paste(rep(DNA_ALPHABET, each=3), collapse='')
+	expect_equal(uniqueLetters(DNAString(d)), DNA_ALPHABET)
+	expect_equal(uniqueLetters(DNAStringSet(c(d,d))), DNA_ALPHABET)
+	expect_equal(uniqueLetters(Views(DNAString(d), start=c(1,6), end=c(5,nchar(d)))), DNA_ALPHABET)
+	md <- DNAString(d)
+	masks(md) <- Mask(nchar(d), start=seq(1,nchar(d),3), end=seq(2,nchar(d),3))
+	expect_equal(uniqueLetters(md), DNA_ALPHABET)
+})
+
+test_that("nucleotideFrequency methods work properly", {
+	set.seed(999L)
+	l <- 102L
+	ds <- sample(DNA_BASES, l, replace=TRUE)
+	d <- DNAString(paste(ds, collapse=''))
+	dss <- DNAStringSet(c(ds,ds))
+
+	## oligonucleotideFrequency(x, 1L) should be the same as alphabetFrequency(...,baseOnly=TRUE)
+	## (ignoring the 'other' arg)
+	expect_equal(oligonucleotideFrequency(d, 1L), alphabetFrequency(d, baseOnly=TRUE)[seq_len(4L)])
+	expect_equal(oligonucleotideFrequency(dss, 1L), alphabetFrequency(dss, baseOnly=TRUE)[,seq_len(4L)])
+
+	## dinucleotide correctness
+	all_doubles <- table(paste0(ds[seq_len(l-1L)], ds[seq_len(l-1L)+1L]))
+	exp_res <- rep(0L, length(DNA_BASES)**2)
+	names(exp_res) <- mkAllStrings(DNA_BASES, 2L)
+	exp_res[names(all_doubles)] <- all_doubles
+	expect_equal(oligonucleotideFrequency(d, 2L), exp_res)
+	expect_equal(dinucleotideFrequency(d), exp_res)
+
+	## trinucleotide correctness
+	all_triples <- table(paste0(ds[seq_len(l-2L)], ds[seq_len(l-2L)+1L], ds[seq_len(l-2L)+2L]))
+	exp_res <- rep(0L, length(DNA_BASES)**3)
+	names(exp_res) <- mkAllStrings(DNA_BASES, 3L)
+	exp_res[names(all_triples)] <- all_triples
+	expect_equal(oligonucleotideFrequency(d, 3L), exp_res)
+	expect_equal(trinucleotideFrequency(d), exp_res)
+
+	## using step
+	all_codons <- codons(d)
+	exp_res[] <- 0L
+	codon_table <- table(as.character(all_codons))
+	exp_res[names(codon_table)] <- codon_table
+	expect_equal(oligonucleotideFrequency(d, 3L, step=3L), exp_res)
+
+	## frequency on a Views object
+	exp_res <- matrix(0L, nrow=l/3, ncol=length(DNA_BASES)**3)
+	colnames(exp_res) <- mkAllStrings(DNA_BASES, 3L)
+	exp_res[cbind(seq_len(nrow(exp_res)), match(as.character(all_codons), colnames(exp_res)))] <- 1L
+	expect_equal(oligonucleotideFrequency(codons(d), 3L), exp_res)
+
+	## frequency on a MaskedXString object
+	mask_d <- d
+	masks(mask_d) <- Mask(l, start=c(10,50), end=c(19,57))
+	mask_str <- strsplit(as.character(mask_d), '')[[1]]
+	all_doubles <- table(paste0(mask_str[seq_len(l-1L)], mask_str[seq_len(l-1L)+1L]))
+	all_doubles <- all_doubles[!grepl("#", names(all_doubles))]
+	exp_res <- rep(0L, length(DNA_BASES)**2)
+	names(exp_res) <- mkAllStrings(DNA_BASES, 2L)
+	exp_res[names(all_doubles)] <- all_doubles
+	expect_equal(oligonucleotideFrequency(mask_d, 2L), exp_res)
+
+	## disallowed types
+	expect_error(oligonucleotideFrequency(AAString(), "must contain sequences of type DNA or RNA"))
+	expect_error(oligonucleotideFrequency(BStringSet(), "must contain sequences of type DNA or RNA"))
 })
