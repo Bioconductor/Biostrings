ebea59ef35a4b4e54c08b3724e3aeb77e5469851
Aidan Lakshman <ahl27@pitt.edu>
2025-02-20 19:30:09 -0500

    Allow custom color palettes for XString objects (#123)
    Add functions update_DNA_palette(), update_RNA_palette(),
    update_AA_palette(), and update_B_palette(), to let users
    set their own color palettes for XString/XStringSet objects.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
 ###
 
+.pkgenv <- new.env(parent=emptyenv())
+
 .onLoad <- function(libname, pkgname)
 {
     .Call2("init_DNAlkups",
@@ -11,8 +13,11 @@
     .Call2("init_AAlkups",
           AA_STRING_CODEC@enc_lkup, AA_STRING_CODEC@dec_lkup,
           PACKAGE=pkgname)
-    DNA_AND_RNA_COLORED_LETTERS <<- make_DNA_AND_RNA_COLORED_LETTERS()
-    AA_COLORED_LETTERS <<- make_AA_COLORED_LETTERS()
+
+    assign("DNA_AND_RNA_COLORED_LETTERS", make_DNA_AND_RNA_COLORED_LETTERS(), envir=.pkgenv)
+    assign("AA_COLORED_LETTERS", make_AA_COLORED_LETTERS(), envir=.pkgenv)
+    assign("B_COLORED_LETTERS", character(0L), envir=.pkgenv)
+
     option_name <- "Biostrings.coloring"
     if (!(option_name %in% names(.Options)))
         options(setNames(list(TRUE), option_name))
--- tests/testthat/test-miscellaneous.R
+++ tests/testthat/test-miscellaneous.R
@@ -2,22 +2,6 @@
 ## these are all relatively low priority and/or for files with only a few things
 ## some tests are just for internal functions
 
-test_that("coloring works for DNA, RNA, and AA", {
-    ## not a super important test
-    make_DNA_AND_RNA_COLORED_LETTERS <-
-        Biostrings:::make_DNA_AND_RNA_COLORED_LETTERS
-    make_AA_COLORED_LETTERS <- Biostrings:::make_AA_COLORED_LETTERS
-
-    dna_rna_expected <- c(DNA_BASES, "U", DNA_ALPHABET[-c(1:4,16:18)])
-    expect_true(!any(duplicated(make_DNA_AND_RNA_COLORED_LETTERS())))
-    expect_equal(sort(names(make_DNA_AND_RNA_COLORED_LETTERS())),
-                 sort(dna_rna_expected))
-
-    aa_expected <- AA_ALPHABET[-c(27:30)]
-    expect_true(!any(duplicated(make_AA_COLORED_LETTERS())))
-    expect_equal(sort(names(make_AA_COLORED_LETTERS())), sort(aa_expected))
-})
-
 test_that("utils functions work as they should", {
     expect_true(Biostrings:::isNumericOrNAs(NA_character_))
     expect_true(Biostrings:::isNumericOrNAs(NA_real_))
