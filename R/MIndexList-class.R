051b30ff6f505c49fc53e44b57e6b291bd18b28f
ahl27 <30053966+ahl27@users.noreply.github.com>
2026-07-06 17:15:28 -0400

    fix formatting

--- R/MIndexList-class.R
+++ R/MIndexList-class.R
@@ -33,44 +33,53 @@ setClass("MIndexList",
     ans_class <- "MIndexList"
 
     ## ensure elements are all MIndex objects
-    if(!all(vapply(from, inherits, logical(1L), what="MIndex")))
+    if (!all(vapply(from, inherits, logical(1L), what="MIndex")))
         stop("Only lists of MIndex objects can be coerced to MIndexList")
     IRanges:::new_CompressedList_from_list(ans_class, from)
 }
 
 ## barebones, but users shouldn't be calling this directly
 ## expecting to be used internally, list coercion provided for QoL
-setAs("list", "MIndexList", function(from) .from_list_to_MIndexList(from))
-setAs("List", "MIndexList", function(from) .from_list_to_MIndexList(as.list(from)))
+setAs("list", "MIndexList",
+    function(from) .from_list_to_MIndexList(from)
+)
+setAs("List", "MIndexList",
+    function(from) .from_list_to_MIndexList(as.list(from))
+)
 
-.from_MIndexList_to_compact_char <- function(object){
-    lapply(object, \(m_ind){
-                paste(length(m_ind), "patterns,",
-                      sum(lengths(m_ind)), "matches")
-            })
+.from_MIndexList_to_compact_char <- function(object)
+{
+    lapply(object,
+        function(m_ind) {
+            paste(length(m_ind), "patterns,",
+                  sum(lengths(m_ind)), "matches")
+        }
+    )
 }
 
 setMethod("show", "MIndexList",
-            function(object) {
-                lo <- length(object)
-                k <- min(5, length(object))
-                diffK <- lo - 5
-                cat(classNameForDisplay(object), " of length ", lo,
-                  "\n", sep = "")
+    function(object)
+    {
+        lo <- length(object)
+        k <- min(5, length(object))
+        diffK <- lo - 5
+        cat(classNameForDisplay(object), " of length ", lo, "\n", sep="")
 
-                repr <- .from_MIndexList_to_compact_char(head(object, k))
-                IRanges:::.showAtomicList(CharacterList(repr), minLines=10L)
-                if (diffK > 0)
-                  cat("...\n<", diffK,
-                      ifelse(diffK == 1,
-                             " more element>\n", " more elements>\n"),
-                      sep="")
-            }
+        repr <- .from_MIndexList_to_compact_char(head(object, k))
+        IRanges:::.showAtomicList(CharacterList(repr), minLines=10L)
+        if (diffK > 0) {
+            cat("...\n<", diffK,
+                ifelse(diffK == 1,
+                       " more element>\n", " more elements>\n"),
+                sep="")
+        }
+    }
 )
 
 setMethod("showAsCell", "MIndexList",
-            function(object){
-                repr <- .from_MIndexList_to_compact_char(object)
-                showAsCell(CharacterList(repr))
-            }
+    function(object)
+    {
+        repr <- .from_MIndexList_to_compact_char(object)
+        showAsCell(CharacterList(repr))
+    }
 )
